1b9b9cc23836076a1470d7587a5beade0760901e
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2018-02-19 20:15:01 -0300

    Removed files.

--- scripts/bio/chain.py
+++
@@ -1,7 +0,0 @@
-from Bio.PDB.Chain import Chain
-
-
-class myChain(Chain):
-
-    def __lt__(self, r2):
-        return self.id < r2.id
--- scripts/bio/residue.py
+++
@@ -1,7 +0,0 @@
-from Bio.PDB.Residue import Residue
-
-
-class myResidue(Residue):
-
-    def __lt__(self, r2):
-        return self.id < r2.id
