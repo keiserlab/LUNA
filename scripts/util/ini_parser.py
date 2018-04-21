232c0c16ae9077343b60bcc92a7ed8320a060d88
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2018-04-21 12:06:00 -0300

    New script for manage .INI files

---
+++ scripts/util/ini_parser.py
@@ -0,0 +1,26 @@
+import configparser
+import logging
+logger = logging.getLogger(__name__)
+
+
+class Config():
+
+    def __init__(self, iniFile):
+        self.config = configparser.ConfigParser()
+        try:
+            self.config.read(iniFile)
+        except Exception as e:
+            logger.exception(e)
+            raise IOError("Configuration file %s not read." % iniFile)
+
+    def get_section_map(self, section):
+        sectionMap = {}
+        options = self.config.options(section)
+        for option in options:
+            try:
+                sectionMap[option] = self.config.get(section, option)
+            except Exception as e:
+                logger.exception(e)
+                logger.warning("Exception on option '%s'!" % option)
+                sectionMap[option] = None
+        return sectionMap
