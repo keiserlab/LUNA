068128282e5536e6af5087c5c26fbfb782a66871
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2018-04-23 10:44:26 -0300

    Only changed a variable name

--- scripts/interaction/contact.py
+++ scripts/interaction/contact.py
@@ -4,30 +4,28 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def all_contacts_nh_search(model, radius=7, level='A'):
-
+def all_contacts_nh_search(entity, radius=7, level='A'):
     try:
         from MyBio.entity_func import get_entity_level_name
         from MyBio.PDB.NeighborSearch import NeighborSearch
 
-        if (level == 'S' or level == 'M' or level == 'C'):
-            raise EntityLevelError("Minimum entity level to be chosen is: "
-                                   "R (residues) or A (atoms)")
+        if level == 'S' or level == 'M' or level == 'C':
+            raise EntityLevelError('Minimum entity level to be chosen is: '
+                                   'R (residues) or A (atoms)')
 
         entitiesName = get_entity_level_name()
-        if (level not in entitiesName):
-            raise EntityLevelError("The defined level %s does not exist"
-                                   % (level))
+        if level not in entitiesName:
+            raise EntityLevelError('The defined level %s does not exist'
+                                   % level)
 
-        logger.info("Trying to select all contacts in the PDB file %s."
-                    % (model.get_parent().id))
+        logger.info('Trying to select all contacts in the PDB file %s.'
+                    % entity.get_parent_by_level('S').id)
 
-        allAtoms = list(model.get_atoms())
+        allAtoms = list(entity.get_atoms())
         ns = NeighborSearch(allAtoms)
-
         pairs = ns.search_all(radius, level)
 
-        logger.info("Number of nearby %s(s) found: %d."
+        logger.info('Number of nearby %s(s) found: %d.'
                     % (entitiesName[level], len(pairs)))
 
         return pairs
@@ -36,44 +34,38 @@ def all_contacts_nh_search(model, radius=7, level='A'):
         raise
 
 
-def get_contacts_for_entity(model, source, target=None, radius=7, level='A'):
-
+def get_contacts_for_entity(entity, source, target=None, radius=7, level='A'):
     try:
         from MyBio.entity_func import get_entity_level_name
-        from MyBio.PDB import (Selection, NeighborSearch)
+        from MyBio.PDB import Selection, NeighborSearch
         from itertools import product
 
-        if (level == 'S' or level == 'M' or level == 'C'):
-            raise EntityLevelError("Minimum entity level to be chosen is: "
-                                   "R (residues) or A (atoms)")
+        if level == 'S' or level == 'M' or level == 'C':
+            raise EntityLevelError('Minimum entity level to be chosen is: '
+                                   'R (residues) or A (atoms)')
 
         entitiesName = get_entity_level_name()
-        if (level not in entitiesName):
-            raise EntityLevelError("The defined level %s does not exist"
-                                   % (level))
+        if level not in entitiesName:
+            raise EntityLevelError('The defined level %s does not exist'
+                                   % level)
 
         sourceAtoms = Selection.unfold_entities([source], 'A')
-
         targetAtoms = []
-        if (target is None):
-            targetAtoms = list(model.get_atoms())
+        if target is None:
+            targetAtoms = list(entity.get_atoms())
         else:
             targetAtoms = Selection.unfold_entities(target, 'A')
 
         ns = NeighborSearch(targetAtoms)
-
         entities = set()
         for atom in sourceAtoms:
             entity = atom.get_parent_by_level(level)
-
             nearbyEntities = ns.search(atom.coord, radius, level)
             pairs = set(product([entity], nearbyEntities))
-
             entities.update(pairs)
 
-        logger.info("Number of nearby %s(s) found: %d."
+        logger.info('Number of nearby %s(s) found: %d.'
                     % (entitiesName[level], len(entities)))
-
         return entities
     except Exception as e:
         logger.exception(e)
