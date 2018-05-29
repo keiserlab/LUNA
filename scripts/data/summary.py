f3b1fae2ce14d392746492729302b73ebf0b684c
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2018-05-29 13:17:00 -0300

    Count interactions according to the informed targets.

--- scripts/data/summary.py
+++ scripts/data/summary.py
@@ -18,25 +18,32 @@ def count_group_types(compound, key_map={}):
     return grp_types
 
 
-def count_interaction_types(interactions, key_map={}):
+def count_interaction_types(interactions, targets=None, key_map={}):
 
     interaction_types = defaultdict(int)
     seen_pairs = set()
     for i in interactions:
-        pair_key1 = (i.type, i.comp1, i.comp2)
-        pair_key2 = (i.type, i.comp2, i.comp1)
+        contain_trgt = True
+        if targets is not None:
+            if (i.comp1.compound not in targets and
+                    i.comp2.compound not in targets):
+                contain_trgt = False
 
-        if pair_key1 in seen_pairs or pair_key2 in seen_pairs:
-            continue
+        if contain_trgt:
+            pair_key1 = (i.type, i.comp1, i.comp2)
+            pair_key2 = (i.type, i.comp2, i.comp1)
 
-        seen_pairs.add(pair_key1)
-        key = i.type
-        if key_map:
-            if i.type in key_map:
-                key = key_map[i.type]
-            else:
+            if pair_key1 in seen_pairs or pair_key2 in seen_pairs:
                 continue
 
-        interaction_types[key] += 1
+            seen_pairs.add(pair_key1)
+            key = i.type
+            if key_map:
+                if i.type in key_map:
+                    key = key_map[i.type]
+                else:
+                    continue
+
+            interaction_types[key] += 1
 
     return interaction_types
