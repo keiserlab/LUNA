6c16e3eec6ab599145c83bf60a089c5c3a095aa3
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2018-07-18 22:43:42 -0300

    All imports were moved to the top

--- scripts/mol/clustering.py
+++ scripts/mol/clustering.py
@@ -1,46 +1,47 @@
-def available_similarity_functions():
-    import re
-    from rdkit import DataStructs
+from rdkit import DataStructs
+from rdkit.ML.Cluster import Butina
+from util.exceptions import IllegalArgumentError
 
+import re
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+def available_similarity_functions():
     regex = re.compile("Bulk([a-zA-Z]+)Similarity", flags=0)
     funcs = list(filter(regex.match, dir(DataStructs)))
 
     return funcs
 
 
-def calc_distance_matrix(fps, similarityFunc="BulkTanimotoSimilarity"):
-    from rdkit import DataStructs
-    from util.exceptions import IllegalArgumentError
-
+def calc_distance_matrix(fps, similarity_func="BulkTanimotoSimilarity"):
     funcs = available_similarity_functions()
-    if (similarityFunc not in funcs):
+    if (similarity_func not in funcs):
         raise IllegalArgumentError("Similarity function not available.")
 
     dists = []
     for i in range(1, len(fps)):
-        if (similarityFunc == "BulkTverskySimilarity"):
+        if (similarity_func == "BulkTverskySimilarity"):
             params = [fps[i], fps[:i], 0, 1]
         else:
             params = [fps[i], fps[:i]]
 
-        sims = getattr(DataStructs, similarityFunc)(*params)
+        sims = getattr(DataStructs, similarity_func)(*params)
         dists.extend([1 - x for x in sims])
 
     return dists
 
 
 def cluster_fps_butina(fps, cutoff=0.2,
-                       similarityFunc="BulkTanimotoSimilarity"):
-    from rdkit.ML.Cluster import Butina
-    import logging
-    logger = logging.getLogger(__name__)
-
+                       similarity_func="BulkTanimotoSimilarity"):
     logger.info("Trying to clusterize %d molecules." % len(fps))
     logger.info("Defined cutoff: %.2f. Defined similarity function: %s."
-                % (cutoff, similarityFunc))
+                % (cutoff, similarity_func))
 
     # first generate the distance matrix:
-    dists = calc_distance_matrix(fps, similarityFunc)
+    dists = calc_distance_matrix(fps, similarity_func)
     logger.info("Distance matrix created.")
 
     # now cluster the data:
