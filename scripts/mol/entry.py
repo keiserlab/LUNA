362d26185f21d55796c5a0ba3f3ca6e25e8ce7ed
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-02-06 15:19:18 -0800

    Major update: added a new entry type (MolEntry) for managing Mol files
    MolEntry allows one to set the molecule that should be analyzed as well as a mol file where the molecule is stored. It is possible to load the file and create a new OBMol object.
    
    I also added a new variable which defines as a regex string how a filename should be. This variable comprises the pattern for evaluating entries whose PDB id is a filename.

--- scripts/mol/entry.py
+++ scripts/mol/entry.py
@@ -1,4 +1,20 @@
+from pybel import (readfile, informats)
+from util.file import get_file_format
+from util.exceptions import (IllegalArgumentError, MoleculeObjectError, MoleculeNotFoundError)
+from os.path import exists
+from MyBio.PDB.PDBParser import PDBParser
+from MyBio.PDB.Entity import Entity
+
 import re
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+# Source: https://richjenks.com/filename-regex/
+FILENAME_REGEX = r"(?!.{256,})(?!(aux|clock\$|con|nul|prn|com[1-9]|lpt[1-9])(?:$|\.))[^ ][ \.\w\-$()+=[\];#@~,&']+[^\. ]"
+
+PDB_PARSER = PDBParser(PERMISSIVE=True, QUIET=True, FIX_ATOM_NAME_CONFLICT=False, FIX_OBABEL_FLAGS=False)
 
 
 class Entry:
@@ -48,9 +64,81 @@ class Entry:
 
 class DBEntry(Entry):
 
-    def __init__(self, ligand_entry_id, pdb, chain, lig_name=None, lig_num=None, lig_icode=None):
+    def __init__(self, ligand_entry_id, pdb_id, chain_id, lig_name=None, lig_num=None, lig_icode=None):
         self.id = ligand_entry_id
-        super().__init__(pdb, chain, lig_name, lig_num, lig_icode)
+        super().__init__(pdb_id, chain_id, lig_name, lig_num, lig_icode)
+
+
+class MolEntry(Entry):
+
+    def __init__(self, pdb_id, mol_id, mol_file, mol_file_type=None):
+        self.mol_id = mol_id
+        self.mol_file = mol_file
+        self.mol_file_type = mol_file_type
+        self._mol_obj = None
+
+        super().__init__(pdb_id, "z", "LIG", 9999)
+
+    @property
+    def mol_obj(self):
+        if self._mol_obj is None:
+            ext = self.mol_file_type or get_file_format(self.mol_file)
+
+            if ext not in informats:
+                raise IllegalArgumentError("Extension '%s' informed or assumed by the filename is not a "
+                                           "recognised Open Babel format." % ext)
+
+            if not exists(self.mol_file):
+                raise FileNotFoundError("The file '%s' was not found." % self.mol_file)
+
+            try:
+                for ob_mol in readfile(ext, self.mol_file):
+                    if self.mol_id == ob_mol.OBMol.GetTitle():
+                        self._mol_obj = ob_mol
+                        break
+            except Exception as e:
+                logger.exception(e)
+                raise MoleculeObjectError("An error occurred while parsing the file with Open Babel and the molecule "
+                                          "object could not be created. Check the logs for more information.")
+
+            if self._mol_obj is None:
+                raise MoleculeNotFoundError("Ligand '%s' not found in the input file." % self.mol_id)
+        return self._mol_obj
+
+    @mol_obj.setter
+    def mol_obj(self, obj):
+        self._mol_obj = obj
+
+    def get_biopython_structure(self, entity=None, parser=PDB_PARSER):
+        pdb_block = self.mol_obj.write('pdb')
+        lig_structure = PDB_PARSER.get_structure_from_pdb_block(self.pdb_id, pdb_block)
+
+        chain = lig_structure[0]['A']
+        if self.chain_id != chain.id:
+            chain.id = self.chain_id
+
+        lig = chain[(" ", 1, " ")]
+        lig.id = ("H_%s" % self.lig_name, self.lig_num, " ")
+        lig.resname = self.lig_name
+
+        if entity is not None:
+            if isinstance(entity, Entity):
+                structure = entity.get_parent_by_level('S')
+                if self.chain_id not in structure[0].child_dict:
+                    chain = chain.copy()
+                    structure[0].add(chain)
+                else:
+                    lig = lig.copy()
+                    structure[0][self.chain_id].add(lig)
+            else:
+                raise IllegalArgumentError("The informed entity is not a valid Biopython object.")
+        else:
+            entity = lig_structure
+
+        return entity
+
+    def __repr__(self):
+        return '<MolEntry: %s%s%s>' % (self.pdb_id, self.separator, self.mol_id)
 
 
 class EntryValidator:
@@ -67,9 +155,10 @@ class PLIEntryValidator(EntryValidator):
 
     def __init__(self, free_filename_format=False):
         if free_filename_format:
-            pattern = '^\\w+(\\w|\\-)*:\\w:\\w{1,3}:\\-?\\d+[a-zA-z]?$'
+            pattern = r'^%s:\w:\w{1,3}:\-?\d{1,4}[a-zA-z]?$' % FILENAME_REGEX
         else:
-            pattern = '^\\w{4}:\\w:\\w{1,3}:\\-?\\d+[a-zA-z]?$'
+            pattern = r'^\w{4}:\w:\w{1,3}:\-?\d{1,4}[a-zA-z]?$'
+
         super().__init__(pattern)
 
 
@@ -81,3 +170,5 @@ class PPIEntryValidator(EntryValidator):
         else:
             pattern = '^\\w{4}:\\w$'
         super().__init__(pattern)
+
+
