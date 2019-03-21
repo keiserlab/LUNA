8c83a57a13814377e86b66c22c0cd8648168409d
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-03-21 15:22:29 -0700

    Major updates: new functions, classes and updates on variable names
    - Class AtomWrapper():
           -> Added function get_id() and get_idx(). The first function returns the unique identifier of an atom and the latter returns its index, i.e., its order in the Mol file.
           -> In many parts of the code I updated the access to an atom by calling the hidden property _atm_obj directly instead of calling the decorated function atm_obj.
           -> New function unwrap to return the original atom object. Same as calling the property atm_obj. The unwrap function was created only for semantic reasons.
    
    - New class BondWrapper to work as wrapper for bonds. It provides functions and access to properties from both RDKit and OpenBabel bond objects.
    
    - MolWrapper:
           -> Function get_atoms() now has the parameter wrapped that controls if it should return original atoms or wrapped atoms.
           -> New function get_bonds() to return all bonds (wrapped or not).
           -> New function get_num_heavy_atoms (self-explanatory).
           -> New function get_atom_coord_by_id returns the coordinates for a specific atom given by its Id.
            -> New function to_smiles to generate a SMILE representation of the molecule.
            -> New function unwrap to return the original molecule object. Same as calling the property mol_obj. The unwrap function was created only for semantic reasons.
             -> In many parts of the code I updated the access to a molecule by calling the hidden property _mol_obj directly instead of calling the decorated function mol_obj.

--- scripts/mol/wrappers/base.py
+++ scripts/mol/wrappers/base.py
@@ -1,7 +1,9 @@
-from util.exceptions import (AtomObjectTypeError, MoleculeObjectTypeError)
+from util.exceptions import AtomObjectTypeError, MoleculeObjectTypeError
 from rdkit.Chem import Atom as RDAtom
 from rdkit.Chem import Mol as RDMol
-from openbabel import (OBMol, OBAtom, OBMolAtomIter, OBAtomAtomIter, OBAtomBondIter)
+from rdkit.Chem import Bond as RDBond
+from rdkit.Chem import MolToSmiles
+from openbabel import OBMol, OBAtom, OBBond, OBMolAtomIter, OBAtomAtomIter, OBAtomBondIter, OBMolBondIter
 from openbabel import etab
 from pybel import Molecule as PybelMol
 
@@ -31,83 +33,147 @@ class AtomWrapper:
         else:
             self._atm_obj = atm_obj
 
-    def get_id(self):
+    def get_idx(self):
         # Both RDKit and Openbabel have the same function name.
-        return self.atm_obj.GetIdx()
+        return self._atm_obj.GetIdx()
+
+    def get_id(self):
+        if self.is_rdkit_obj():
+            return self._atm_obj.GetIdx()
+        elif self.is_openbabel_obj():
+            return self._atm_obj.GetId()
 
     def get_atomic_num(self):
         # Both RDKit and Openbabel have the same function name.
-        return self.atm_obj.GetAtomicNum()
+        return self._atm_obj.GetAtomicNum()
 
     def get_symbol(self):
         if self.is_rdkit_obj():
-            return self.atm_obj.GetSymbol()
+            return self._atm_obj.GetSymbol()
         elif self.is_openbabel_obj():
-            return etab.GetSymbol(self.atm_obj.GetAtomicNum())
+            return etab.GetSymbol(self._atm_obj.GetAtomicNum())
 
     def get_valence(self):
         if self.is_rdkit_obj():
-            return self.atm_obj.GetExplicitValence() + self.atm_obj.GetImplicitValence()
+            return self._atm_obj.GetExplicitValence() + self._atm_obj.GetImplicitValence()
         elif self.is_openbabel_obj():
-            bonds = [b.GetBondOrder() for b in OBAtomBondIter(self.atm_obj)]
-            return sum(bonds) + self.atm_obj.ImplicitHydrogenCount()
+            bonds = [b.GetBondOrder() for b in OBAtomBondIter(self._atm_obj)]
+            return sum(bonds) + self._atm_obj.ImplicitHydrogenCount()
 
     def get_implicit_valence(self):
         # Both RDKit and Openbabel have the same function name.
-        return self.atm_obj.GetImplicitValence()
+        return self._atm_obj.GetImplicitValence()
 
     def get_neighbors(self):
         if self.is_rdkit_obj():
-            return self.atm_obj.GetNeighbors()
+            return self._atm_obj.GetNeighbors()
         elif self.is_openbabel_obj():
-            return OBAtomAtomIter(self.atm_obj)
+            return OBAtomAtomIter(self._atm_obj)
 
     def get_degree(self):
         if self.is_rdkit_obj():
             # In RDKit, GetDegree() returns the degree of the atom.
-            return self.atm_obj.GetDegree()
+            return self._atm_obj.GetDegree()
         elif self.is_openbabel_obj():
             # In OpenBabel, GetValence() returns the current number of explicit connections (degree).
-            return self.atm_obj.GetValence()
+            return self._atm_obj.GetValence()
 
     def get_implicit_h_count(self):
         if self.is_rdkit_obj():
-            return self.atm_obj.GetNumImplicitHs()
+            return self._atm_obj.GetNumImplicitHs()
         elif self.is_openbabel_obj():
-            return self.atm_obj.ImplicitHydrogenCount()
+            return self._atm_obj.ImplicitHydrogenCount()
 
     def get_explicit_h_count(self):
         if self.is_rdkit_obj():
-            return self.atm_obj.GetNumExplicitHs()
+            return self._atm_obj.GetNumExplicitHs()
         elif self.is_openbabel_obj():
-            return self.atm_obj.ExplicitHydrogenCount()
+            return self._atm_obj.ExplicitHydrogenCount()
 
     def get_charge(self):
         # Both RDKit and Openbabel have the same function name.
-        return self.atm_obj.GetFormalCharge()
+        return self._atm_obj.GetFormalCharge()
 
     def set_charge(self, charge):
         # Both RDKit and Openbabel have the same function name.
-        self.atm_obj.SetFormalCharge(charge)
+        self._atm_obj.SetFormalCharge(charge)
 
     def set_implicit_valence(self, valence):
         if self.is_rdkit_obj():
             logger.warning("RDKit does not provide a setter for implicit valence.")
         elif self.is_openbabel_obj():
-            self.atm_obj.SetImplicitValence(valence)
+            self._atm_obj.SetImplicitValence(valence)
+
+    def unwrap(self):
+        return self._atm_obj
 
     def is_rdkit_obj(self):
-        if isinstance(self.atm_obj, RDAtom):
+        if isinstance(self._atm_obj, RDAtom):
             return True
         return False
 
     def is_openbabel_obj(self):
-        if isinstance(self.atm_obj, OBAtom):
+        if isinstance(self._atm_obj, OBAtom):
             return True
         return False
 
     def __getattr__(self, attr):
-        return getattr(self.atm_obj, attr)
+        return getattr(self._atm_obj, attr)
+
+
+class BondWrapper:
+    def __init__(self, bond_obj):
+        if not isinstance(bond_obj, RDBond) and not isinstance(bond_obj, OBBond):
+            logger.exception("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
+            raise MoleculeObjectTypeError("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
+
+        self._bond_obj = bond_obj
+
+    @property
+    def bond_obj(self):
+        return self._bond_obj
+
+    @bond_obj.setter
+    def bond_obj(self, bond_obj):
+        if not isinstance(bond_obj, RDBond) and not isinstance(bond_obj, OBBond):
+            logger.exception("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
+            raise AtomObjectTypeError("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
+        else:
+            self._bond_obj = bond_obj
+
+    def get_begin_atom(self, wrapped=False):
+        # Both RDKit and Openbabel have the same function name.
+        if wrapped:
+            return AtomWrapper(self._bond_obj.GetBeginAtom())
+        return self._bond_obj.GetBeginAtom()
+
+    def get_end_atom(self, wrapped=False):
+        # Both RDKit and Openbabel have the same function name.
+        if wrapped:
+            return AtomWrapper(self._bond_obj.GetEndAtom())
+        return self._bond_obj.GetEndAtom()
+
+    def get_bond_type(self):
+        if self.is_rdkit_obj():
+            return self._bond_obj.GetBondType()
+        elif self.is_openbabel_obj():
+            return self._bond_obj.GetBondOrder()
+
+    def unwrap(self):
+        return self._bond_obj
+
+    def is_rdkit_obj(self):
+        if isinstance(self._bond_obj, RDBond):
+            return True
+        return False
+
+    def is_openbabel_obj(self):
+        if isinstance(self._bond_obj, OBBond):
+            return True
+        return False
+
+    def __getattr__(self, attr):
+        return getattr(self._bond_obj, attr)
 
 
 class MolWrapper:
@@ -136,25 +202,67 @@ class MolWrapper:
 
         self._mol_obj = mol_obj
 
-    def get_atoms(self):
+    def get_atoms(self, wrapped=False):
         if self.is_rdkit_obj():
-            return self.mol_obj.GetAtoms()
+            atoms = self._mol_obj.GetAtoms()
         elif self.is_openbabel_obj():
-            return OBMolAtomIter(self.mol_obj)
+            atoms = OBMolAtomIter(self._mol_obj)
+
+        if atoms:
+            if wrapped:
+                return [AtomWrapper(atm) for atm in atoms]
+            return atoms
+
+    def get_bonds(self, wrapped=False):
+        if self.is_rdkit_obj():
+            bonds = self._mol_obj.GetBonds()
+        elif self.is_openbabel_obj():
+            bonds = OBMolBondIter(self._mol_obj)
+
+        if bonds:
+            if wrapped:
+                return [BondWrapper(bond) for bond in bonds]
+            return bonds
+        return []
+
+    def get_num_heavy_atoms(self):
+        if self.is_rdkit_obj():
+            return self._mol_obj.GetNumHeavyAtoms()
+        elif self.is_openbabel_obj():
+            return self._mol_obj.NumHvyAtoms()
+
+    def get_atom_coord_by_id(self, atom_id):
+        if self.is_rdkit_obj():
+            return list(self._mol_obj.GetConformer().GetAtomPosition(atom_id))
+        elif self.is_openbabel_obj():
+            atm = self._mol_obj.GetAtomById(atom_id)
+            return [atm.GetX(), atm.GetY(), atm.GetZ()]
+
+    def to_smiles(self):
+        if self.is_rdkit_obj():
+            return MolToSmiles(self._mol_obj)
+        elif self.is_openbabel_obj():
+            return PybelMol(self._mol_obj).write("smi").split("\t")[0]
+
+    def unwrap(self):
+        return self._mol_obj
 
     def is_rdkit_obj(self):
-        if isinstance(self.mol_obj, RDMol):
+        if isinstance(self._mol_obj, RDMol):
             return True
         return False
 
     def is_openbabel_obj(self):
-        if isinstance(self.mol_obj, OBMol):
+        if isinstance(self._mol_obj, OBMol):
             return True
-        elif isinstance(self.mol_obj, PybelMol):
+        elif isinstance(self._mol_obj, PybelMol):
             return True
         return False
 
     def is_pybel_obj(self):
-        if isinstance(self.mol_obj, PybelMol):
+        if isinstance(self._mol_obj, PybelMol):
             return True
         return False
+
+    def __getattr__(self, attr):
+        return getattr(self._mol_obj, attr)
