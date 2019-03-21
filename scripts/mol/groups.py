bc9d65e39ec58e8453c522f3299dade3339db8da
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-03-21 15:47:17 -0700

    Now we perceive residue/ligand properties from both RDKit and OpenBabel objects
    To perceive residue/ligand properties from both RDKit and OpenBabel objects we use the wrappers provided by mol.wrappers.base
    
    The function _get_mol_from_entity() first converts a PDB object to PDB file and then to MOL file as previously. But then, it can return either an OpenBabel or RDKit object. The molecules may also be validated using the MolValidator class

--- scripts/mol/groups.py
+++ scripts/mol/groups.py
@@ -2,16 +2,18 @@ from MyBio.selector import ResidueSelector
 from MyBio.util import save_to_file
 
 import mol.interaction.math as im
+
 from mol.interaction.contact import get_contacts_for_entity
 from mol.neighborhood import (NbAtom, NbAtomData)
 from mol.charge_model import OpenEyeModel
-from mol.validator import (MolValidator, RDKitValidator)
+from mol.validator import MolValidator
 from mol.wrappers.obabel import convert_molecule
+from mol.wrappers.base import MolWrapper
 from util.file import get_unique_filename
-from util.exceptions import (MoleculeSizeError, MoleculeInformationError, IllegalArgumentError)
+from util.exceptions import MoleculeSizeError, IllegalArgumentError, MoleculeObjectError
+from util.default_values import ACCEPTED_MOL_OBJ_TYPES
 
-from rdkit.Chem import MolFromMolBlock
-from openbabel import (OBMolAtomIter, OBMolBondIter)
+from rdkit.Chem import MolFromMolBlock, MolFromMolFile, SanitizeFlags, SanitizeMol
 from pybel import readfile
 
 from collections import defaultdict
@@ -182,33 +184,29 @@ class CompoundGroups():
         self.atm_grps = list(atm_grps)
 
 
-# TODO:
-# Option: use openbabel or rdkit
 class CompoundGroupPerceiver():
 
-    def __init__(self, feature_extractor, add_h=False, ph=None, error_level=4,
+    def __init__(self, feature_extractor, add_h=False, ph=None, amend_mol=True, mol_obj_type="rdkit",
                  charge_model=OpenEyeModel(), tmp_path=None, expand_selection=True, radius=COV_CUTOFF):
-        '''
-            error_level:
-                0 = permissive mode. No validation will be performed.
-                1 = amending mode. A validation will be performed and it will try to fix some errors.
-                2 = strict mode. A validation will be performed, and no errors will be accepted.
-                3 = RDKit sanitization mode. A validation will be performed using RDKit sanitization functions, and no errors will be accepted.
-                4 = Amending + RDKit sanitization mode. A validation will be performed using RDKit sanitization functions, but first it will try to fix errors.
-        '''
+
+        if mol_obj_type not in ACCEPTED_MOL_OBJ_TYPES:
+            raise IllegalArgumentError("Objects of type '%s' are not currently accepted. "
+                                       "The available options are: %s." % (mol_obj_type, ", ".join(ACCEPTED_MOL_OBJ_TYPES)))
+
         self.feature_extractor = feature_extractor
         self.add_h = add_h
         self.ph = ph
-        self.error_level = error_level
+        self.amend_mol = amend_mol
+        self.mol_obj_type = mol_obj_type
         self.charge_model = charge_model
         self.tmp_path = tmp_path
         self.expand_selection = expand_selection,
         self.radius = radius
 
-    def perceive_compound_groups(self, target_residue, ob_mol=None, only_grps_with_target=True):
+    def perceive_compound_groups(self, target_residue, mol_obj=None, only_grps_with_target=True):
         # If no OBMol object was defined and expand_selection was set ON, it will get all residues around the
         # target and create a new OBMol object with them.
-        if ob_mol is None and self.expand_selection:
+        if mol_obj is None and self.expand_selection:
             res_list = self._get_all_around_residue(target_residue)
         # Otherwise, the residue list will be composed only by the target residue.
         else:
@@ -217,48 +215,47 @@ class CompoundGroupPerceiver():
         # Select residues based on the residue list defined previously.
         res_sel = ResidueSelector(res_list, keep_altloc=False, keep_hydrog=False)
         # If no OBMol was defined, create a new one through the residue list.
-        ob_mol = ob_mol or self._get_obmol_from_entity(target_residue.get_parent_by_level('M'), res_sel)
-
-        # Validate ob_mol object according to the error_level informed when initializing this class.
-        self._validate_obmol(ob_mol)
+        mol_obj = mol_obj or self._get_mol_from_entity(target_residue.get_parent_by_level('M'), res_sel)
 
         # Recover all atoms from the previous selected residues.
         atoms = [a for r in res_list for a in r.get_unpacked_list() if res_sel.accept_atom(a)]
         atoms = tuple(sorted(atoms, key=lambda a: a.serial_number))
 
-        if ob_mol.OBMol.NumHvyAtoms() != len(atoms):
+        mol_obj = MolWrapper(mol_obj)
+
+        if mol_obj.get_num_heavy_atoms() != len(atoms):
             raise MoleculeSizeError("The number of heavy atoms in the PDB selection and in the MOL file are different.")
 
         # Ignore hydrogen atoms
-        ob_atms = [atm for atm in OBMolAtomIter(ob_mol.OBMol) if atm.GetAtomicNum() != 1]
+        atm_obj_list = [atm for atm in mol_obj.get_atoms(wrappered=True) if atm.get_atomic_num() != 1]
 
         atm_map = {}
         trgt_atms = {}
-        for i, ob_atm in enumerate(ob_atms):
-            atm_map[ob_atm.GetIdx()] = atoms[i].serial_number
+        for i, atm_obj in enumerate(atm_obj_list):
+            atm_map[atm_obj.get_idx()] = atoms[i].serial_number
             trgt_atms[atoms[i].serial_number] = NbAtom(atoms[i])
 
         # Set all neighbors, i.e., covalently bonded atoms.
-        for ob_bond in OBMolBondIter(ob_mol.OBMol):
-            bgn_ob_atm = ob_bond.GetBeginAtom()
-            end_ob_atm = ob_bond.GetEndAtom()
+        for bond_obj in mol_obj.get_bonds(wrappered=True):
+            bgn_atm_obj = bond_obj.get_begin_atom(wrappered=True)
+            end_atm_obj = bond_obj.get_end_atom(wrappered=True)
 
             # At least one of the atoms must be a non-hydrogen atom.
-            if bgn_ob_atm.GetAtomicNum() != 1 or end_ob_atm.GetAtomicNum() != 1:
+            if bgn_atm_obj.get_atomic_num() != 1 or end_atm_obj.get_atomic_num() != 1:
                 # If the atom 1 is not a hydrogen, add atom 2 to its neighbor list.
-                if bgn_ob_atm.GetAtomicNum() != 1:
-                    serial_number = atm_map.get(end_ob_atm.GetIdx())
-                    coord = [end_ob_atm.GetX(), end_ob_atm.GetY(), end_ob_atm.GetZ()]
-                    atom_info = NbAtomData(end_ob_atm.GetAtomicNum(), coord, serial_number)
-                    trgt_atms[atm_map[bgn_ob_atm.GetIdx()]].add_nb_atom(atom_info)
+                if bgn_atm_obj.get_atomic_num() != 1:
+                    serial_number = atm_map.get(end_atm_obj.get_idx())
+                    coord = mol_obj.get_atom_coord_by_id(end_atm_obj.get_id())
+                    atom_info = NbAtomData(end_atm_obj.get_atomic_num(), coord, serial_number)
+                    trgt_atms[atm_map[bgn_atm_obj.get_idx()]].add_nb_atom(atom_info)
                 # If the atom 2 is not a hydrogen, add atom 1 to its neighbor list.
-                if end_ob_atm.GetAtomicNum() != 1:
-                    serial_number = atm_map.get(bgn_ob_atm.GetIdx())
-                    coord = [bgn_ob_atm.GetX(), bgn_ob_atm.GetY(), bgn_ob_atm.GetZ()]
-                    atom_info = NbAtomData(bgn_ob_atm.GetAtomicNum(), coord, serial_number)
-                    trgt_atms[atm_map[end_ob_atm.GetIdx()]].add_nb_atom(atom_info)
+                if end_atm_obj.get_atomic_num() != 1:
+                    serial_number = atm_map.get(bgn_atm_obj.get_idx())
+                    coord = mol_obj.get_atom_coord_by_id(bgn_atm_obj.get_id())
+                    atom_info = NbAtomData(bgn_atm_obj.get_atomic_num(), coord, serial_number)
+                    trgt_atms[atm_map[end_atm_obj.get_idx()]].add_nb_atom(atom_info)
 
-        group_features = self.feature_extractor.get_features_by_groups(ob_mol, atm_map)
+        group_features = self.feature_extractor.get_features_by_groups(mol_obj, atm_map)
 
         comp_grps = CompoundGroups(target_residue, only_grps_with_target=only_grps_with_target)
         for key in group_features:
@@ -282,63 +279,71 @@ class CompoundGroupPerceiver():
 
         return res_list
 
-    def _get_obmol_from_entity(self, entity, residue_selector):
+    def _get_mol_from_entity(self, entity, residue_selector):
         # First it saves the selection into a PDB file and then it converts the file to .mol.
         # I had to do it because the OpenBabel 2.4.1 had a problem with some molecules containing aromatic rings.
         # In such cases, the aromatic ring was being wrongly perceived and some atoms received more double bonds than it
-        # was exepcted. The version 2.3.2 works fine. Therefore, I defined this version manually (openbabel property).
+        # was expected. The version 2.3.2 works better. Therefore, I defined this version manually (openbabel property).
         filename = get_unique_filename(self.tmp_path)
         pdb_file = '%s.pdb' % filename
+        logger.info("Saving the PDB object as a PDB file named '%s'." % pdb_file)
         save_to_file(entity, pdb_file, residue_selector)
 
         mol_file = '%s.mol' % filename
         ob_opt = {"error-level": 5}
+        logger.info("Converting the PDB file '%s' to a Mol file named '%s' using Open Babel." % (pdb_file, mol_file))
         if self.add_h:
+            logger.info("Hydrogens will be added to the molecule.")
             if self.ph is not None:
                 ob_opt["p"] = self.ph
             else:
                 ob_opt["h"] = ""
         convert_molecule(pdb_file, mol_file, opt=ob_opt, openbabel='/usr/bin/obabel')
-        # Read molecule file and return an OBMol object.
-        return next(readfile("mol", mol_file))
 
-    def _validate_obmol(self, ob_mol):
-        if self.error_level == 0:
-            logger.warning("Error level set to 0. No validation will be performed. "
-                           "Molecules containing errors may generate incorrect results.")
-        elif self.error_level == 1:
-            logger.warning("Error level set to 1. A validation will be performed and it will try to fix some errors.")
+        if self.amend_mol:
+            logger.info("A validation will be performed and it will try to fix some errors.")
+
+            try:
+                mol_obj = next(readfile("mol", mol_file))
+            except Exception as e:
+                logger.exception(e)
+                raise MoleculeObjectError("An error occurred while parsing the file '%s' with Open Babel and the molecule "
+                                          "object could not be created. Check the logs for more information." % mol_file)
+
             mv = MolValidator(fix_charges=True, fix_implicit_valence=True)
-            if not mv.is_mol_valid(ob_mol):
-                raise MoleculeInformationError("Informed molecule '%s' is invalid. "
-                                               "Check the logs for more information." % ob_mol.OBMol.GetTitle())
-        elif self.error_level == 2:
-            logger.warning("Error level set to 2. A validation will be performed and no errors will be accepted.")
-            mv = MolValidator()
-            if not mv.is_mol_valid(ob_mol):
-                raise MoleculeInformationError("Informed molecule '%s' is invalid. "
-                                               "Check the logs for more information." % ob_mol.OBMol.GetTitle())
-        elif self.error_level == 3:
-            logger.warning("Error level set to 3. A validation will be performed using RDKit sanitization functions. "
-                           "No errors will be accepted.")
-            mv = RDKitValidator()
-            rdk_mol = MolFromMolBlock(ob_mol.write('mol'), removeHs=False, sanitize=False)
-            if not mv.is_mol_valid(rdk_mol):
-                raise MoleculeInformationError("Informed molecule '%s' is invalid. "
-                                               "Check the logs for more information." % ob_mol.OBMol.GetTitle())
-        elif self.error_level == 4:
-            logger.warning("A validation will be performed using RDKit sanitization functions. "
-                           "No errors will be accepted.")
-            mv = MolValidator(fix_charges=True, fix_implicit_valence=True)
-            if not mv.is_mol_valid(ob_mol):
-                raise MoleculeInformationError("Informed molecule '%s' is invalid. "
-                                               "Check the logs for more information." % ob_mol.OBMol.GetTitle())
+            is_valid = mv.validate_mol(mol_obj)
+            logger.info('Validation finished!!!')
+            if not is_valid:
+                logger.warning("The molecular file '%s' contain invalid atoms. Check the logs for more information." % mol_file)
+
+            if self.mol_obj_type == "openbabel":
+                return mol_obj
             else:
-                mv = RDKitValidator()
-                rdk_mol = MolFromMolBlock(ob_mol.write('mol'), removeHs=False, sanitize=False)
-                if not mv.is_mol_valid(rdk_mol):
-                    raise MoleculeInformationError("Informed molecule '%s' is invalid. "
-                                                   "Check the logs for more information." % ob_mol.OBMol.GetTitle())
+                try:
+                    # The sanitization is set off. We will apply it in the next statement.
+                    rdk_mol = MolFromMolBlock(mol_obj.write('mol'), sanitize=False, removeHs=False)
+                    # Sanitize molecule is applied now, so we will be able to catch the exceptions raised by RDKit,
+                    # otherwise it would not be possible.
+                    SanitizeMol(rdk_mol, SanitizeFlags.SANITIZE_ALL)
+                    return rdk_mol
+                except Exception as e:
+                    logger.exception(e)
+                    raise MoleculeObjectError("An error occurred while parsing the molecular block generated by Open Babel with "
+                                              "RDKit. Check the logs for more information." % mol_file)
         else:
-            raise IllegalArgumentError("Error level must be either 0, 1, 2, 3, or 4.")
-        logger.warning('Molecule validated!!!')
+            try:
+                # Create a new Mol object.
+                if self.mol_obj_type == "openbabel":
+                    return next(readfile("mol", mol_file))
+                else:
+                    # The sanitization is set off. We will apply it in the next statement.
+                    rdk_mol = MolFromMolFile(mol_file, sanitize=False, removeHs=False)
+                    # Sanitize molecule is applied now, so we will be able to catch the exceptions raised by RDKit,
+                    # otherwise it would not be possible.
+                    SanitizeMol(rdk_mol, SanitizeFlags.SANITIZE_ALL)
+                    return rdk_mol
+            except Exception as e:
+                logger.exception(e)
+                tool = "Open Babel" if self.mol_obj_type == "openbabel" else "RDKit"
+                raise MoleculeObjectError("An error occurred while parsing the file '%s' with %s and the molecule "
+                                          "object could not be created. Check the logs for more information." % (mol_file, tool))
