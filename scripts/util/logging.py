32c93d92e957e2259424f7c2d650607bed9d66c2
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-03-21 15:35:38 -0700

    Function new_logging_file() now returns a RotatingFileHandler and new function load_default_logging_conf
    The new function load_default_logging_conf() clear all existing handlers and creates new handlers according to the default configuration file.

--- scripts/util/logging.py
+++ scripts/util/logging.py
@@ -1,4 +1,6 @@
 import logging
+import logging.config
+import os.path
 
 
 DEFAULT_FORMAT = '%(asctime)s - %(processName)s - %(name)s - %(levelname)s - %(filename)s - %(funcName)s - line %(lineno)d => %(message)s'
@@ -6,17 +8,32 @@ DEFAULT_FORMAT = '%(asctime)s - %(processName)s - %(name)s - %(levelname)s - %(f
 
 def new_logging_file(filename, log_format=DEFAULT_FORMAT, mode='a'):
     # Set the LOG file at the working path
-    fh = logging.FileHandler(filename, mode)
+    fh = logging.handlers.RotatingFileHandler(filename, mode, 5 * 1024 * 1024, 100)
     formatter = logging.Formatter(log_format)
     fh.setFormatter(formatter)
+    fh.setLevel(logging.INFO)
+
+    ch = logging.StreamHandler()
+    ch.setFormatter(formatter)
+    ch.setLevel(logging.ERROR)
 
     logger = logging.getLogger()
 
     # Remove the existing file handlers
     for hdlr in logger.handlers[:]:
-        if isinstance(hdlr, logging.FileHandler):
-            logger.removeHandler(hdlr)
-    # Set the new handler
+        logger.removeHandler(hdlr)
+
+    # Set the new handlers
     logger.addHandler(fh)
-    # Set the log level to INFO, DEBUG as the default is ERROR
-    logger.setLevel("INFO")
+    logger.addHandler(ch)
+
+
+def load_default_logging_conf():
+    logger = logging.getLogger()
+
+    # Remove the existing file handlers
+    for hdlr in logger.handlers[:]:
+        logger.removeHandler(hdlr)
+
+    LOGGING_CONF = os.path.join(os.path.dirname(__file__), "logging.ini")
+    logging.config.fileConfig(LOGGING_CONF)
