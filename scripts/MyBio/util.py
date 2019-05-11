384261a5a36946af2941c9781c34790f5386a63c
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-05-11 13:18:45 -0700

    Major updates on download_pdb(), get_residue_neighbors(), get_residue_cov_bonds()
    - Removed the parameter output_file from download_pdb(). Now, all files are renamed to have the extension '.pdb'.
    
    - New function get_residue_cov_bonds() to recover atoms covalently bonded to a target residue.
    
    - The statements used to verify if two atoms are covalently bonded were removed and substituted to the new function is_covalently_bonded() added in the calc.py script.
    
    - The statements that recover a residue index were removed as the residues now have this information internally.

--- scripts/MyBio/util.py
+++ scripts/MyBio/util.py
@@ -1,9 +1,10 @@
 import logging
-from openbabel import etab
 from io import StringIO
+from os.path import exists
 from shutil import move as rename_pdb_file
-from itertools import product
+from itertools import product, combinations
 
+from mol.interaction.calc import is_covalently_bonded
 from util.exceptions import IllegalArgumentError, MoleculeNotFoundError, ChainNotFoundError
 from util.file import is_directory_valid
 from util.default_values import ENTRY_SEPARATOR
@@ -17,7 +18,7 @@ from util.exceptions import FileNotCreated, PDBNotReadError
 logger = logging.getLogger()
 
 
-def download_pdb(pdb_id, output_path=".", output_file=None, overwrite=False):
+def download_pdb(pdb_id, output_path=".", overwrite=False):
     """Download a PDB file from RCSB.org.
 
         @param pdb_id: 4-symbols structure Id from PDB (e.g. 3J92).
@@ -26,26 +27,28 @@ def download_pdb(pdb_id, output_path=".", output_file=None, overwrite=False):
         @param output_path: put the PDB file in this directory.
         @type  output_path: string
     """
-    logger.info("Trying to download the PDB '%s' and store it at the directory '%s'."
-                % (pdb_id, output_path))
+    logger.info("Trying to download the PDB '%s' and store it at the directory '%s'." % (pdb_id, output_path))
 
     try:
-        pdb_id = pdb_id.lower()
-        if (pdb_id is not None and pdb_id.strip() != ""):
-            if (is_directory_valid(output_path)):
-                pdbl = PDBList()
-                pdbl.retrieve_pdb_file(pdb_id, pdir=output_path, file_format="pdb", overwrite=overwrite)
+        if pdb_id is not None and pdb_id.strip() != "":
+            if is_directory_valid(output_path):
+                new_pdb_file = "%s/%s.pdb" % (output_path, pdb_id)
+                # If the file already exists and is not to overwrite the file, do nothing.
+                if overwrite is False and not exists(new_pdb_file):
+                    pdbl = PDBList()
+                    pdbl.retrieve_pdb_file(pdb_id, pdir=output_path, file_format="pdb", overwrite=overwrite)
+                    logger.info("Download completed!!")
 
-                if output_file:
-                    pdb_file = '%s/pdb%s.ent' % (output_path, pdb_id)
-                    rename_pdb_file(pdb_file, output_file)
+                    # Rename files.
+                    cur_pdb_file = '%s/pdb%s.ent' % (output_path, pdb_id.lower())
+                    rename_pdb_file(cur_pdb_file, new_pdb_file)
+                else:
+                    logger.info("File already exists. It will not be downloaded again.")
         else:
-            raise IllegalArgumentError("Inform a non empty PDB id")
+            raise IllegalArgumentError("An empty PDB id ('%s') was informed." % pdb_id)
     except Exception as e:
         logger.exception(e)
         raise
-
-    logger.info("Download complete!!")
 
 
 def parse_from_file(id, file):
@@ -102,8 +105,7 @@ def entity_to_string(entity, select=Select(), write_conects=True, write_end=True
     fh = StringIO()
     io = PDBIO()
     io.set_structure(entity)
-    io.save(fh, select=select, write_conects=write_conects, write_end=write_end,
-            preserve_atom_numbering=preserve_atom_numbering)
+    io.save(fh, select=select, write_conects=write_conects, write_end=write_end, preserve_atom_numbering=preserve_atom_numbering)
     fh.seek(0)
     return ''.join(fh.readlines())
 
@@ -119,40 +121,45 @@ def get_entity_level_name():
 
 
 def get_entity_from_entry(entity, entry, model=0):
-        structure = entity.get_parent_by_level("S")
-        model = structure[model]
+    structure = entity.get_parent_by_level("S")
+    model = structure[model]
 
-        if entry.chain_id in model.child_dict:
-            chain = model[entry.chain_id]
+    if entry.chain_id in model.child_dict:
+        chain = model[entry.chain_id]
 
-            if entry.comp_name and entry.comp_num:
-                ligand_key = entry.get_biopython_key()
-                if ligand_key in chain.child_dict:
-                    target_entity = chain[ligand_key]
-                else:
-                    raise MoleculeNotFoundError("Ligand '%s' does not exist in the PDB '%s'."
-                                                % (entry.to_string(ENTRY_SEPARATOR), structure.get_id()))
+        if entry.comp_name and entry.comp_num:
+            ligand_key = entry.get_biopython_key()
+            if ligand_key in chain.child_dict:
+                target_entity = chain[ligand_key]
             else:
-                target_entity = chain
+                raise MoleculeNotFoundError("Ligand '%s' does not exist in the PDB '%s'."
+                                            % (entry.to_string(ENTRY_SEPARATOR), structure.get_id()))
         else:
-            raise ChainNotFoundError("The informed chain id '%s' for the ligand entry '%s' does not exist in the PDB '%s'." %
-                                     (entry.chain_id, entry.to_string(ENTRY_SEPARATOR), structure.get_id()))
+            target_entity = chain
+    else:
+        raise ChainNotFoundError("The informed chain id '%s' for the ligand entry '%s' does not exist in the PDB '%s'." %
+                                 (entry.chain_id, entry.to_string(ENTRY_SEPARATOR), structure.get_id()))
 
-        return target_entity
+    return target_entity
 
 
-def get_residue_neighbors(residue, select=Select()):
-
-    # TODO: add index as a property of the residue.
-    for idx, res in enumerate(residue.parent.child_list):
-        if residue == res:
-            break
+def get_residue_cov_bonds(residue, select=Select()):
 
     # Get valid atoms according to the provided selection function.
     trgt_res_atms = {atm.name: atm for atm in residue.get_atoms() if select.accept_atom(atm)}
 
-    print(trgt_res_atms.keys())
-    print()
+    cov_bonds = []
+    for atm1, atm2 in combinations(trgt_res_atms.values(), 2):
+        if is_covalently_bonded(atm1, atm2):
+            cov_bonds.append((atm1, atm2))
+
+    return cov_bonds
+
+
+def get_residue_neighbors(residue, select=Select()):
+
+    # Get valid atoms according to the provided selection function.
+    trgt_res_atms = {atm.name: atm for atm in residue.get_atoms() if select.accept_atom(atm)}
 
     if residue.is_aminoacid():
         if "N" not in trgt_res_atms:
@@ -168,21 +175,15 @@ def get_residue_neighbors(residue, select=Select()):
 
         neighbors = {}
         # If the chain has a residue coming before the target residue.
-        if idx - 1 >= 0:
+        if residue.idx - 1 >= 0:
             # First residue before the target in the chain list.
-            prev_res = residue.parent.child_list[idx - 1]
+            prev_res = residue.parent.child_list[residue.idx - 1]
             # Get valid atoms according to the provided selection function.
             prev_res_atms = {atm.name: atm for atm in prev_res.get_atoms() if select.accept_atom(atm)}
 
             # A peptide bond exists between the C of one amino acid and the N of another.
             if "C" in prev_res_atms and "N" in trgt_res_atms:
-                # Distance atom-atom
-                dist = trgt_res_atms["N"] - prev_res_atms["C"]
-                # Covalent radius
-                cov1 = etab.GetCovalentRad(etab.GetAtomicNum(trgt_res_atms["N"].element))
-                cov2 = etab.GetCovalentRad(etab.GetAtomicNum(prev_res_atms["C"].element))
-                # OpenBabel thresholds.
-                if 0.4 <= dist <= cov1 + cov2 + 0.45:
+                if is_covalently_bonded(trgt_res_atms["N"], prev_res_atms["C"]):
                     neighbors["previous"] = prev_res
                 else:
                     logger.warning("The first residue before %s is too distant to fulfill the covalent thresholds. "
@@ -197,22 +198,15 @@ def get_residue_neighbors(residue, select=Select()):
                            "It may be the first in the chain sequence or there are missing residues." % residue)
 
         # If the chain has a residue coming after the target residue.
-        if idx + 1 < len(residue.parent.child_list):
+        if residue.idx + 1 < len(residue.parent.child_list):
             # First residue after the target in the chain list.
-            next_res = residue.parent.child_list[idx + 1]
+            next_res = residue.parent.child_list[residue.idx + 1]
             # Get valid atoms according to the provided selection function.
             next_res_atms = {atm.name: atm for atm in next_res.get_atoms() if select.accept_atom(atm)}
 
             # A peptide bond exists between the C of one amino acid and the N of another.
             if "C" in trgt_res_atms and "N" in next_res_atms:
-                # Distance atom-atom
-                dist = next_res_atms["N"] - trgt_res_atms["C"]
-                # Covalent radius
-                cov1 = etab.GetCovalentRad(etab.GetAtomicNum(next_res_atms["N"].element))
-                cov2 = etab.GetCovalentRad(etab.GetAtomicNum(trgt_res_atms["C"].element))
-
-                # OpenBabel thresholds.
-                if 0.4 <= dist <= cov1 + cov2 + 0.45:
+                if is_covalently_bonded(trgt_res_atms["C"], next_res_atms["N"]):
                     neighbors["next"] = next_res
                 else:
                     logger.warning("The first residue after %s is too distant to fulfill the covalent thresholds. "
@@ -220,37 +214,29 @@ def get_residue_neighbors(residue, select=Select()):
             elif "N" in next_res_atms:
                 logger.warning("There is a missing N in %s, the first residue after %s in the chain list. "
                                "It may have been filtered out by the provided selection function. "
-                               "So, the predecessor residue cannot be identified." % (prev_res, residue))
+                               "So, the predecessor residue cannot be identified." % (next_res, residue))
         # Otherwise, it could mean that the residue is the last one in the sequence or there are missing residues.
         else:
 
             logger.warning("The residue %s seems not to have any successor residue. "
                            "It may be the last in the chain sequence or there are missing residues." % residue)
-
         return neighbors
     else:
         # First residue before the target in the chain list.
-        prev_res = residue.parent.child_list[idx - 1]
+        prev_res = residue.parent.child_list[residue.idx - 1]
         # Get valid atoms according to the provided selection function.
         prev_res_atms = {atm.name: atm for atm in prev_res.get_atoms() if select.accept_atom(atm)}
 
         neighbors = {}
         # If the chain has a residue coming before the target residue.
-        if idx - 1 >= 0:
+        if residue.idx - 1 >= 0:
             # First residue before the target in the chain list.
-            prev_res = residue.parent.child_list[idx - 1]
+            prev_res = residue.parent.child_list[residue.idx - 1]
             # Get valid atoms according to the provided selection function.
             prev_res_atms = {atm.name: atm for atm in prev_res.get_atoms() if select.accept_atom(atm)}
 
-            for prev_atm, trgt_atm in product(prev_res_atms.values(), trgt_res_atms.values()):
-                # Distance atom-atom
-                dist = trgt_atm - prev_atm
-                # Covalent radius
-                cov1 = etab.GetCovalentRad(etab.GetAtomicNum(trgt_atm.element))
-                cov2 = etab.GetCovalentRad(etab.GetAtomicNum(prev_atm.element))
-
-                # OpenBabel thresholds.
-                if 0.4 <= dist <= cov1 + cov2 + 0.45:
+            for trgt_atm, prev_atm in product(trgt_res_atms.values(), prev_res_atms.values()):
+                if is_covalently_bonded(trgt_atm, prev_atm):
                     neighbors["previous"] = prev_res
                     break
 
@@ -263,22 +249,15 @@ def get_residue_neighbors(residue, select=Select()):
                            "It may be the first in the chain sequence or there are missing residues." % residue)
 
         # If the chain has a residue coming after the target residue.
-        if idx + 1 < len(residue.parent.child_list):
+        if residue.idx + 1 < len(residue.parent.child_list):
             # First residue after the target in the chain list.
-            next_res = residue.parent.child_list[idx + 1]
+            next_res = residue.parent.child_list[residue.idx + 1]
             # Get valid atoms according to the provided selection function.
             next_res_atms = {atm.name: atm for atm in next_res.get_atoms() if select.accept_atom(atm)}
 
             # Check each pair of atoms for covalently bonded atoms.
-            for next_atm, trgt_atm in product(next_res_atms.values(), trgt_res_atms.values()):
-                # Distance atom-atom
-                dist = trgt_atm - next_atm
-                # Covalent radius
-                cov1 = etab.GetCovalentRad(etab.GetAtomicNum(trgt_atm.element))
-                cov2 = etab.GetCovalentRad(etab.GetAtomicNum(next_atm.element))
-
-                # OpenBabel thresholds.
-                if 0.4 <= dist <= cov1 + cov2 + 0.45:
+            for trgt_atm, next_atm in product(trgt_res_atms.values(), next_res_atms.values()):
+                if is_covalently_bonded(trgt_atm, next_atm):
                     neighbors["next"] = next_res
                     break
 
@@ -289,5 +268,5 @@ def get_residue_neighbors(residue, select=Select()):
         else:
             logger.warning("The residue %s seems not to have any successor residue. "
                            "It may be the last in the chain sequence or there are missing residues." % residue)
-
         return neighbors
+    return {}
