bf43c914d25e3ff5df0018bc89046c35591bc65e
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-05-11 10:47:05 -0700

    Added information to the new interactions and added new variables
    - Variable COV_SEARCH_RADIUS defines a radius to search for covalently bonded atoms.
    - Variable IFP_LENGTH defines the default IFP length.
    - Variable ACCEPTED_MOL_OBJ_TYPES limits which molecular objects are accepted in the current version of the library.

--- scripts/util/default_values.py
+++ scripts/util/default_values.py
@@ -18,6 +18,12 @@ BOUNDARY_CONF = InteractionConf({"boundary_cutoff": 7})
 
 NMR_METHODS = ["SOLID-STATE NMR", "SOLUTION NMR"]
 
+COV_SEARCH_RADIUS = 2.2
+
+IFP_LENGTH = 1024
+
+ACCEPTED_MOL_OBJ_TYPES = ("rdkit", "openbabel")
+
 ATOM_TYPES_COLOR = ColorPallete({
     "Acceptor": (252, 141, 89),
     "Donor": (145, 191, 219),
@@ -30,24 +36,6 @@ ATOM_TYPES_COLOR = ColorPallete({
     "HalogenAcceptor": (215, 48, 3),
     "HalogenDonor": (215, 48, 3),
 }, (255, 255, 255))
-
-PYMOL_INTERACTION_COLOR = ColorPallete({
-    "Proximal": "gray60",
-    "Hydrogen bond": "tv_blue",
-    "Water-bridged hydrogen bond": "lightblue",
-    "Weak hydrogen bond": "lightteal",
-    "Attractive": "forest",
-    "Salt bridge": "palegreen",
-    "Cation-pi": "salmon",
-    "Edge-to-face pi-stacking": "tv_red",
-    "Face-to-face pi-stacking": "tv_red",
-    "Parallel-displaced pi-stacking": "tv_red",
-    "Hydrophobic": "orange",
-    "Halogen bond": "aquamarine",
-    "Repulsive": "violetpurple",
-    "Pi-stacking": "tv_red",
-    "Covalent bond": "black"
-}, "white")
 
 CHEMICAL_FEATURE_IDS = {
     "Aromatic": 1,
@@ -75,7 +63,7 @@ CHEMICAL_FEATURE_IDS = {
 INTERACTION_IDS = {
     "Proximal": 0,
     "Hydrogen bond": 1,
-    "Attractive": 2,
+    "Ionic": 2,
     "Salt bridge": 3,
     "Cation-pi": 4,
     "Edge-to-face pi-stacking": 5,
@@ -91,13 +79,27 @@ INTERACTION_IDS = {
     "Covalent bond": 15,
     "Atom overlap": 16,
     "Van der Waals clash": 17,
-    "Van der Waals": 18
+    "Van der Waals": 18,
+    "Chalcogen bond": 19,
+    "Chalcogen-pi": 20,
+    "Halogen-pi": 21,
+    "Orthogonal multipolar": 22,
+    "Parallel multipolar": 23,
+    "Antiparallel multipolar": 24,
+    "Tilted multipolar": 25,
+    "Multipolar": 26,
+    "Cation-nucleophile": 27,
+    "Anion-electrophile": 28,
+    "Unfavorable anion-nucleophile": 29,
+    "Unfavorable cation-electrophile": 30,
+    "Unfavorable nucleophile-nucleophile": 31,
+    "Unfavorable electrophile-electrophile": 32,
 }
 
 INTERACTION_SHORT_NAMES = {
     "Proximal": "prox",
     "Hydrogen bond": "hbond",
-    "Attractive": "attrac",
+    "Ionic": "ionic",
     "Salt bridge": "salt_bridge",
     "Cation-pi": "cation-pi",
     "Edge-to-face pi-stacking": "t-shaped_pi-stack",
@@ -105,17 +107,62 @@ INTERACTION_SHORT_NAMES = {
     "Parallel-displaced pi-stacking": "disp_pi-stack",
     "Hydrophobic": "hphobe",
     "Halogen bond": "xbond",
+    "Halogen bond": "x-pi",
+    "Chalcogen bond": "ybond",
+    "Chalcogen-pi": "y-pi",
     "Repulsive": "repuls",
     "Water-bridged hydrogen bond": "water_hbond",
     "Pi-stacking": "pi-stack",
     "Amide-aromatic stacking": "amide_pi-stack",
     "Weak hydrogen bond": "weak_hbond",
+    "Orthogonal multipolar": "ort_multipol",
+    "Parallel multipolar": "par_multipol",
+    "Antiparallel multipolar": "antipar_multipol",
+    "Tilted multipolar": "tilted_multipol",
+    "Multipolar": "multipol",
+    "Unfavorable nucleophile-nucleophile": "unf_nucleop_nucleop",
+    "Unfavorable electrophile-electrophile": "unf_electrop_electrop",
+    "Cation-nucleophile": "cat_nucleop",
+    "Anion-electrophile": "ani_electrop",
+    "Unfavorable anion-nucleophile": "unf_ani_nucleop",
+    "Unfavorable cation-electrophile": "unf_cat_electrop",
     "Covalent bond": "cov",
     "Atom overlap": "atm_overlap",
     "Van der Waals clash": "vdw_clash",
-    "Van der Waals": "vdw"
+    "Van der Waals": "vdw",
 }
 
-IFP_LENGTH = 1024
-
-ACCEPTED_MOL_OBJ_TYPES = ("rdkit", "openbabel")
+PYMOL_INTERACTION_COLOR = ColorPallete({
+    "Proximal": "gray60",
+    "Hydrogen bond": "tv_blue",
+    "Water-bridged hydrogen bond": "lightblue",
+    "Weak hydrogen bond": "lightteal",
+    "Ionic": "green",
+    "Salt bridge": "forest",
+    "Cation-pi": "salmon",
+    "Edge-to-face pi-stacking": "tv_red",
+    "Face-to-face pi-stacking": "tv_red",
+    "Parallel-displaced pi-stacking": "tv_red",
+    "Pi-stacking": "tv_red",
+    "Amide-aromatic stacking": "raspberry",
+    "Hydrophobic": "orange",
+    "Halogen bond": "aquamarine",
+    "Halogen-pi": "aquamarine",
+    "Chalcogen bond": "lightorange",
+    "Chalcogen-pi": "lightorange",
+    "Repulsive": "violetpurple",
+    "Covalent bond": "black",
+    "Van der Waals clash": "gray90",
+    "Van der Waals": "gray50",
+    "Orthogonal multipolar": "paleyellow",
+    "Parallel multipolar": "paleyellow",
+    "Antiparallel multipolar": "paleyellow",
+    "Tilted multipolar": "paleyellow",
+    "Multipolar": "paleyellow",
+    "Unfavorable nucleophile-nucleophile": "dirtyviolet",
+    "Unfavorable electrophile-electrophile": "dirtyviolet",
+    "Cation-nucleophile": "palegreen",
+    "Anion-electrophile": "palegreen",
+    "Unfavorable anion-nucleophile": "dirtyviolet",
+    "Unfavorable cation-electrophile": "dirtyviolet"
+}, "white")
