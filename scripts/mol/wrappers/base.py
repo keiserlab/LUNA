37837c01914cfa7876e49b4011164da356db2046
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-07-04 09:23:25 -0300

    Methods update
    Updates in AtomWrapper:
    
      New methods:
          - get_isotope(): recovers an atom isotope number.
          - get_mass(): recovers an atom mass given its isotope number.
          - get_atomic_mass(): recovers the atom mass according to the periodic table and considering the default mass of an atom.
          - get_neighbors_number(): returns the number of neighbors from an atom. It also can return the number of  neighbors that are heavy atoms.
          - get_bonds(): returns the bonds of an atom.
          - get_h_count(): returns the total number of Hydrogens. No implicit or explicit hydrogen count is available anymore.
    
      Updates in the old methods:
          - get_degree(): now returns the total degree of an atom.
          - get_implicit_valence(): removed
          - get_implicit_h_count(): removed
          - get_explicit_h_count(): removed
          - __init__(): now accepts a AtomWrapper object. In this case, the object is first unwrapped and a new AtomWrapper is created.
          - Now, the value of all parameters "wrapped" is True by default;
    
    Updates in BondWrapper:
    
      New methods:
          - get_partner_atom(): given a atom passed as parameter, it returns its partner in a bond.
          - set_bond_type(): allows one to set the bond type.
    
      Updates in the old methods:
          - __init__(): now accepts a AtomWrapper object. In this case, the object is first unwrapped and a new AtomWrapper is created.
          - Now, the value of all parameters "wrapped" is True by default;
    
    Updates in MolWrapper:
    
      Updates in the old methods:
          - __init__(): now accepts a AtomWrapper object. In this case, the object is first unwrapped and a new AtomWrapper is created.
          - Now, the value of all parameters "wrapped" is True by default;

--- scripts/mol/wrappers/base.py
+++ scripts/mol/wrappers/base.py
@@ -2,7 +2,7 @@ from util.exceptions import AtomObjectTypeError, MoleculeObjectTypeError
 from rdkit.Chem import Atom as RDAtom
 from rdkit.Chem import Mol as RDMol
 from rdkit.Chem import Bond as RDBond
-from rdkit.Chem import MolToSmiles
+from rdkit.Chem import MolToSmiles, GetPeriodicTable
 from openbabel import OBMol, OBAtom, OBBond, OBMolAtomIter, OBAtomAtomIter, OBAtomBondIter, OBMolBondIter
 from openbabel import etab
 from pybel import Molecule as PybelMol
@@ -15,6 +15,9 @@ logger = logging.getLogger()
 class AtomWrapper:
 
     def __init__(self, atm_obj):
+        if isinstance(atm_obj, self.__class__):
+            atm_obj = atm_obj.unwrap()
+
         if not isinstance(atm_obj, RDAtom) and not isinstance(atm_obj, OBAtom):
             logger.exception("Objects of type '%s' are not currently accepted." % atm_obj.__class__)
             raise AtomObjectTypeError("Objects of type '%s' are not currently accepted." % atm_obj.__class__)
@@ -53,18 +56,35 @@ class AtomWrapper:
         elif self.is_openbabel_obj():
             return etab.GetSymbol(self._atm_obj.GetAtomicNum())
 
-    def get_valence(self):
-        if self.is_rdkit_obj():
-            return self._atm_obj.GetExplicitValence() + self._atm_obj.GetImplicitValence()
-        elif self.is_openbabel_obj():
-            bonds = [b.GetBondOrder() for b in OBAtomBondIter(self._atm_obj)]
-            return sum(bonds) + self._atm_obj.ImplicitHydrogenCount()
-
-    def get_implicit_valence(self):
+    def get_charge(self):
         # Both RDKit and Openbabel have the same function name.
-        return self._atm_obj.GetImplicitValence()
+        return self._atm_obj.GetFormalCharge()
+
+    def get_isotope(self):
+        if self.is_rdkit_obj():
+            return int(round(self._atm_obj.GetMass()))
+        elif self.is_openbabel_obj():
+            return int(round(self._atm_obj.GetExactMass()))
+
+    def get_mass(self):
+        if self.is_rdkit_obj():
+            return self._atm_obj.GetMass()
+        elif self.is_openbabel_obj():
+            return self._atm_obj.GetExactMass()
+
+    def get_atomic_mass(self):
+        if self.is_rdkit_obj():
+            return GetPeriodicTable().GetAtomicWeight(self.get_atomic_num())
+        elif self.is_openbabel_obj():
+            return self._atm_obj.GetAtomicMass()
+
+    def get_neighbors_number(self, only_heavy_atoms=False):
+        # Subtract the number of hydrogens from the degree if only heavy atoms must be considered.
+        penalty = 0 if only_heavy_atoms is False else self.get_h_count()
+        return self.get_degree() - penalty
 
     def get_neighbors(self):
+        # TODO: wrap option
         if self.is_rdkit_obj():
             return self._atm_obj.GetNeighbors()
         elif self.is_openbabel_obj():
@@ -73,26 +93,35 @@ class AtomWrapper:
     def get_degree(self):
         if self.is_rdkit_obj():
             # In RDKit, GetDegree() returns the degree of the atom.
-            return self._atm_obj.GetDegree()
+            return self._atm_obj.GetTotalDegree()
         elif self.is_openbabel_obj():
-            # In OpenBabel, GetValence() returns the current number of explicit connections (degree).
-            return self._atm_obj.GetValence()
+            # In OpenBabel, GetImplicitValence() returns the maximum number of connections expected (degree).
+            return self._atm_obj.GetImplicitValence()
 
-    def get_implicit_h_count(self):
+    def get_valence(self):
         if self.is_rdkit_obj():
-            return self._atm_obj.GetNumImplicitHs()
+            return self._atm_obj.GetExplicitValence() + self._atm_obj.GetImplicitValence()
         elif self.is_openbabel_obj():
-            return self._atm_obj.ImplicitHydrogenCount()
+            bonds = [b.GetBondOrder() for b in OBAtomBondIter(self._atm_obj)]
+            return sum(bonds) + self._atm_obj.ImplicitHydrogenCount()
 
-    def get_explicit_h_count(self):
+    def get_h_count(self):
         if self.is_rdkit_obj():
-            return self._atm_obj.GetNumExplicitHs()
+            return self._atm_obj.GetTotalNumHs(includeNeighbors=True)
         elif self.is_openbabel_obj():
-            return self._atm_obj.ExplicitHydrogenCount()
+            return self._atm_obj.ImplicitHydrogenCount() + self._atm_obj.ExplicitHydrogenCount()
 
-    def get_charge(self):
-        # Both RDKit and Openbabel have the same function name.
-        return self._atm_obj.GetFormalCharge()
+    def get_bonds(self, wrapped=True):
+        bonds = []
+        if self.is_rdkit_obj():
+            # TODO: IMPLEMENT
+            bonds = []
+        elif self.is_openbabel_obj():
+            bonds = OBAtomBondIter(self._atm_obj)
+
+        if bonds and wrapped:
+            return [BondWrapper(bond) for bond in bonds]
+        return bonds
 
     def set_charge(self, charge):
         # Both RDKit and Openbabel have the same function name.
@@ -123,6 +152,9 @@ class AtomWrapper:
 
 class BondWrapper:
     def __init__(self, bond_obj):
+        if isinstance(bond_obj, self.__class__):
+            bond_obj = bond_obj.unwrap()
+
         if not isinstance(bond_obj, RDBond) and not isinstance(bond_obj, OBBond):
             logger.exception("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
             raise MoleculeObjectTypeError("Objects of type '%s' are not currently accepted." % bond_obj.__class__)
@@ -141,13 +173,27 @@ class BondWrapper:
         else:
             self._bond_obj = bond_obj
 
-    def get_begin_atom(self, wrapped=False):
+    def get_partner_atom(self, atm, wrapped=True):
+        if isinstance(atm, AtomWrapper):
+            atm = atm.unwrap()
+
+        partner = None
+        if atm == self._bond_obj.GetBeginAtom():
+            partner = self._bond_obj.GetEndAtom()
+        elif atm == self._bond_obj.GetEndAtom():
+            partner = self._bond_obj.GetBeginAtom()
+
+        if partner and wrapped:
+            return AtomWrapper(partner)
+        return partner
+
+    def get_begin_atom(self, wrapped=True):
         # Both RDKit and Openbabel have the same function name.
         if wrapped:
             return AtomWrapper(self._bond_obj.GetBeginAtom())
         return self._bond_obj.GetBeginAtom()
 
-    def get_end_atom(self, wrapped=False):
+    def get_end_atom(self, wrapped=True):
         # Both RDKit and Openbabel have the same function name.
         if wrapped:
             return AtomWrapper(self._bond_obj.GetEndAtom())
@@ -158,6 +204,13 @@ class BondWrapper:
             return self._bond_obj.GetBondType()
         elif self.is_openbabel_obj():
             return self._bond_obj.GetBondOrder()
+
+    def set_bond_type(self, bond_type):
+        if self.is_rdkit_obj():
+            pass
+            # return self._bond_obj.GetBondType()
+        elif self.is_openbabel_obj():
+            return self._bond_obj.SetBondOrder(bond_type)
 
     def unwrap(self):
         return self._bond_obj
@@ -178,7 +231,9 @@ class BondWrapper:
 
 class MolWrapper:
     def __init__(self, mol_obj):
-        if isinstance(mol_obj, PybelMol):
+        if isinstance(mol_obj, self.__class__):
+            mol_obj = mol_obj.unwrap()
+        elif isinstance(mol_obj, PybelMol):
             mol_obj = mol_obj.OBMol
 
         if not isinstance(mol_obj, RDMol) and not isinstance(mol_obj, OBMol):
@@ -202,29 +257,29 @@ class MolWrapper:
 
         self._mol_obj = mol_obj
 
-    def get_atoms(self, wrapped=False):
+    def get_atoms(self, wrapped=True):
+
+        atoms = []
         if self.is_rdkit_obj():
             atoms = self._mol_obj.GetAtoms()
         elif self.is_openbabel_obj():
             atoms = OBMolAtomIter(self._mol_obj)
 
-        if atoms:
-            if wrapped:
-                return [AtomWrapper(atm) for atm in atoms]
-            return atoms
-        return []
+        if atoms and wrapped:
+            return [AtomWrapper(atm) for atm in atoms]
+        return atoms
 
-    def get_bonds(self, wrapped=False):
+    def get_bonds(self, wrapped=True):
+
+        bonds = []
         if self.is_rdkit_obj():
             bonds = self._mol_obj.GetBonds()
         elif self.is_openbabel_obj():
             bonds = OBMolBondIter(self._mol_obj)
 
-        if bonds:
-            if wrapped:
-                return [BondWrapper(bond) for bond in bonds]
-            return bonds
-        return []
+        if bonds and wrapped:
+            return [BondWrapper(bond) for bond in bonds]
+        return bonds
 
     def get_num_heavy_atoms(self):
         if self.is_rdkit_obj():
@@ -232,11 +287,11 @@ class MolWrapper:
         elif self.is_openbabel_obj():
             return self._mol_obj.NumHvyAtoms()
 
-    def get_atom_coord_by_id(self, atom_id):
+    def get_atom_coord_by_id(self, atm_id):
         if self.is_rdkit_obj():
-            return list(self._mol_obj.GetConformer().GetAtomPosition(atom_id))
+            return list(self._mol_obj.GetConformer().GetAtomPosition(atm_id))
         elif self.is_openbabel_obj():
-            atm = self._mol_obj.GetAtomById(atom_id)
+            atm = self._mol_obj.GetAtomById(atm_id)
             return [atm.GetX(), atm.GetY(), atm.GetZ()]
 
     def to_smiles(self):
