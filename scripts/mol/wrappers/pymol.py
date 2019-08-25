adbbb344c31f0b1acef0b078147bc17a551eafa9
Alexandre Fassio <alexandrefassiogcc@gmail.com>
2019-08-25 18:13:42 -0300

    Function mybio_to_pymol_selection was not return anything if not a Atom or Residue

--- scripts/mol/wrappers/pymol.py
+++ scripts/mol/wrappers/pymol.py
@@ -336,26 +336,25 @@ def mybio_to_pymol_selection(entity):
     params = {}
     if entity.level == 'S' or entity.level == 'M':
         params[''] = 'all'
+    elif entity.level == 'C':
+        params['chain'] = entity.id
+    elif entity.level == 'R':
+            params['resn'] = entity.resname
+            params['res'] = str(entity.id[1]) + entity.id[2].strip()
+            params['chain'] = entity.get_parent().id
+    elif entity.level == 'A':
+            residue = entity.get_parent()
+            params['id'] = entity.serial_number
+            params['name'] = entity.name
+            params['resn'] = residue.resname
+            params['res'] = str(residue.id[1]) + residue.id[2].strip()
+            params['chain'] = residue.get_parent().id
     else:
-        if entity.level == 'C':
-            params['chain'] = entity.id
-        else:
-            if entity.level == 'R':
-                params['resn'] = entity.resname
-                params['res'] = str(entity.id[1]) + entity.id[2].strip()
-                params['chain'] = entity.get_parent().id
-            else:
-                if entity.level == 'A':
-                    residue = entity.get_parent()
-                    params['id'] = entity.serial_number
-                    params['name'] = entity.name
-                    params['resn'] = residue.resname
-                    params['res'] = str(residue.id[1]) + residue.id[2].strip()
-                    params['chain'] = residue.get_parent().id
+        return {}
 
-            if "resn" in params:
-                # Escape characters that can generate problems with Pymol
-                params['resn'] = params['resn'].replace("+", "\\+")
-                params['resn'] = params['resn'].replace("-", "\\-")
+    if "resn" in params:
+        # Escape characters that can generate problems with Pymol
+        params['resn'] = params['resn'].replace("+", "\\+")
+        params['resn'] = params['resn'].replace("-", "\\-")
 
-            return (' AND ').join(['%s %s' % (k, str(v)) for k, v in params.items()])
+    return (' AND ').join(['%s %s' % (k, str(v)) for k, v in params.items()])
