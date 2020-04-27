87d14d03e8645dac37357acc4bbe2652b6b13fa7
Alexandre Fassio <alexandrefassio@dcc.ufmg.br>
2020-04-27 16:03:14 -0300

    Added a log-like string to mimic the LOG format

--- luna/util/progress_tracker.py
+++ luna/util/progress_tracker.py
@@ -5,6 +5,7 @@ from threading import Thread, Event
 
 import time
 import sys
+from colorlog.escape_codes import escape_codes, parse_colors
 
 
 import logging
@@ -54,10 +55,22 @@ class ProgressTracker:
             if self.task_name:
                 task_name = " - %s" % self.task_name
 
-            # print the current progress value in bar and percent form
-            sys.stdout.write('\r%s%% [%s] %d/%d [Avg: %.2fs/task]%s' % (int(perc), ("\u25A0" * int(perc / 2)).ljust(50, ' '), p,
-                                                                        self.ntasks, self.avg_running_time, task_name))
+            msg = '%s%% [%s] %d/%d [Avg: %.2fs/task]%s' % (int(perc), ("\u25A0" * int(perc / 2)).ljust(50, ' '),
+                                                           p, self.ntasks, self.avg_running_time, task_name)
+
+            # CONSOLE_FORMAT = '[%(asctime)s]    %(log_color)s%(levelname)-10s %(reset)s%(filename)16s:%(lineno)-10d %(message)s'
+
+            format_str = '\r[%s]    %s%s %s%s  %s'
+            progress_str = format_str % (time.strftime('%Y-%m-%d %H:%M:%S'), parse_colors("red"),
+                                         "PROGRESS".ljust(10, " "), escape_codes["reset"], "".rjust(26, " "), msg)
+
+            sys.stdout.write(progress_str)
             sys.stdout.flush()
+
+            # print the current progress value in bar and percent form
+            # sys.stdout.write('\r%s%% [%s] %d/%d [Avg: %.2fs/task]%s' % (int(perc), ("\u25A0" * int(perc / 2)).ljust(50, ' '), p,
+            #                                                             self.ntasks, self.avg_running_time, task_name))
+            # sys.stdout.flush()
 
     @property
     def progress(self):
