2eac165eabf0aaccef32416c6d044f21e6c34a4c
Alexandre Fassio <alexandrefassio@dcc.ufmg.br>
2020-07-28 15:53:55 -0300

    The function set_view() now highlights all hydrophobic atoms if possible
    The function set_view() now tries to recover an AtomGroupManager from one of the interacting atom groups. If so, it will be able to highlight all nearby hydrophobes interacting or not. By doing so, it has the potential to reveal which non-polar atoms are not satisfied.
    
    If the program fails at recovering an AtomGroupsManager, then it will proceed by highlighting only interacting hydrophobes.

--- luna/mol/interaction/view.py
+++ luna/mol/interaction/view.py
@@ -33,17 +33,44 @@ class InteractionViewer(PymolSessionManager):
             if self.show_hydrop_surface:
                 self.wrapper.color([("white", "%s and !name PS*" % main_grp)])
 
-                interacting_atms = set()
-                for inter in interactions:
-                    if inter.type != "Hydrophobic":
-                        continue
+                # It will display all hydrophobic groups if a AtomGroupsManager is available.
+                atm_grp_mngr = interactions[0].src_grp.manager
+                if atm_grp_mngr is not None:
+                    interacting_atms = set()
+                    for atm_grp in atm_grp_mngr.filter_by_types(["Hydrophobe", "Hydrophobic"], must_contain_all=False):
+                        inters = [i for i in atm_grp.interactions if i.type == "Hydrophobic"]
 
-                    for atm in inter.src_grp.atoms + inter.trgt_grp.atoms:
-                        if atm not in interacting_atms:
-                            self.wrapper.color([("pink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+                        for comp in atm_grp.compounds:
+                            comp_sel = mybio_to_pymol_selection(comp)
+                            self.wrapper.show([("sticks", comp_sel)])
 
-                    for atm in inter.src_interacting_atms + inter.trgt_interacting_atms:
-                        self.wrapper.color([("hotpink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
-                        interacting_atms.add(atm)
+                        if len(inters) > 0:
+                            for inter in inters:
+                                for atm in inter.src_grp.atoms + inter.trgt_grp.atoms:
+                                    if atm not in interacting_atms:
+                                        self.wrapper.color([("pink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+
+                                for atm in inter.src_interacting_atms + inter.trgt_interacting_atms:
+                                    self.wrapper.color([("hotpink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+                                    interacting_atms.add(atm)
+
+                        else:
+                            for atm in atm_grp.atoms:
+                                self.wrapper.color([("wheat", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+
+                # Otherwise, it will display only hydrophobic groups comprising the interacting groups.
+                else:
+                    interacting_atms = set()
+                    for inter in interactions:
+                        if inter.type != "Hydrophobic":
+                            continue
+
+                        for atm in inter.src_grp.atoms + inter.trgt_grp.atoms:
+                            if atm not in interacting_atms:
+                                self.wrapper.color([("pink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+
+                        for atm in inter.src_interacting_atms + inter.trgt_interacting_atms:
+                            self.wrapper.color([("hotpink", "%s and %s" % (main_grp, mybio_to_pymol_selection(atm)))])
+                            interacting_atms.add(atm)
 
         self.set_last_details_to_view()
