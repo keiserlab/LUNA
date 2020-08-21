22ab4b9b42926edd2df7ab1c3ff8bc060ec20b71
Alexandre Fassio <alexandrefassio@dcc.ufmg.br>
2020-08-21 17:37:25 -0300

    Remove examples from the main script path as it will be place elsewhere

--- luna/examples/fetch_pdbs.py
+++
@@ -1,9 +0,0 @@
-from MyBio.PDB.PDBList import PDBList
-
-listToDownload = ["1H2T", "1eve", "1P5E", "3PFP", "1WBG", "1R9O"]
-
-outPath = "../tmp/pharm"
-
-pdbl = PDBList()
-for pdbId in listToDownload:
-    pdbl.retrieve_pdb_file(pdbId, pdir=outPath, file_format="pdb")
