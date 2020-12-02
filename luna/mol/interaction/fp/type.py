463057fc234d0ef0fbbf8347427cd73b88de33e8
Alexandre Fassio <alexandrefassio@dcc.ufmg.br>
2020-12-02 12:51:40 -0300

    Changed IFP name from EIFP_WITH_PHARM_FOR_GROUPS to HIFP

--- luna/mol/interaction/fp/type.py
+++ luna/mol/interaction/fp/type.py
@@ -5,5 +5,5 @@ class IFPType(Enum):
     # auto() creates automatic identifiers for each type as this value is not important.
     # Therefore, always remember to compare IFP types using their names and not their values.
     EIFP = auto()
-    EIFP_WITH_PHARM_FOR_GROUPS = auto()
+    HIFP = auto()  # Hybrid version: atomic invariants for atoms and pharmacophore for groups
     FIFP = auto()
