b6f0ad1ca92de462b6470bbcb21f37720c0121af
Unknown <alexandrefassio@ufmg.br>
2021-06-14 01:47:01 -0300

    Added new options to the main run script.
    Now it is possible to filter binding modes by providing a configuration file.
    
    It is possible to decide to overwrite a project and output PSE files and similarity matrices.

--- luna/run.py
+++ luna/run.py
@@ -3,6 +3,7 @@ import os
 import sys
 from os import path
 
+
 scripts = path.abspath(path.join(path.realpath(__file__), '../..'))
 sys.path.append(scripts)
 
@@ -10,11 +11,10 @@ from luna import LocalProject
 from luna.version import __version__ as version
 
 from luna.mol.entry import MolEntry
-from luna.mol.interaction.filter import InteractionFilter
+from luna.mol.interaction.filter import InteractionFilter, BindingModeFilter
 from luna.mol.interaction.calc import InteractionCalculator
 from luna.mol.interaction.fp.type import IFPType
-
-from luna.util.file import get_filename
+from luna.util.file import get_filename, create_directory
 
 
 IFP_TYPES = {
@@ -25,7 +25,6 @@ IFP_TYPES = {
 
 
 def get_entries(args):
-
     with open(args.entries_file, "r") as IN:
         entry_ids = set()
         for row in IN:
@@ -73,10 +72,23 @@ def main():
     parser.add_argument('-O', dest="ifp_output", type=str,
                         help="the fingerprint output file. Default: <WORKING_PATH>/results/fingerprints/ifp.csv")
 
+    parser.add_argument('--filter_binding_modes', dest="binding_modes_file", type=str,
+                        help="the path of a file containing binding modes to filter")
+
+    parser.add_argument('--overwrite', dest="overwrite", action="store_true",
+                        help="defines whether it should overwrite an existing project")
+
+    parser.add_argument('--out_pse', dest="out_pse", action="store_true",
+                        help="defines whether it should export interactions to Pymol")
+
+    parser.add_argument('--out_sim', dest="out_sim", action="store_true",
+                        help="defines whether it should generate a similarity matrix")
+
     args = parser.parse_args()
 
     proj_pkl_file = "%s/project_v%s.pkl.gz" % (args.working_path, version)
-    if not os.path.exists(proj_pkl_file):
+
+    if not os.path.exists(proj_pkl_file) or args.overwrite:
         print(u"\u25a8 Creating a new LUNA project: '%s'...\n" % args.working_path)
 
         entries = list(get_entries(args))
@@ -88,11 +100,16 @@ def main():
         opt["working_path"] = args.working_path
         opt["pdb_path"] = pdb_path
         opt["entries"] = entries
-        opt["overwrite_path"] = False
+        opt["overwrite_path"] = args.overwrite
         opt["inter_calc"] = ic
         opt["mol_obj_type"] = 'rdkit'
         opt["try_h_addition"] = True
         opt["amend_mol"] = True
+        opt["calc_ifp"] = False
+        opt["out_pse"] = args.out_pse
+
+        if args.binding_modes_file:
+            opt["binding_mode_filter"] = BindingModeFilter.from_config_file(args.binding_modes_file)
 
         pli_obj = LocalProject(**opt)
         pli_obj.run()
@@ -103,7 +120,8 @@ def main():
 
     ifp_output = args.ifp_output
     if ifp_output is None:
-        ifp_output = "%s/results/fingerprints/ifp.csv" % (args.working_path)
+        create_directory("%s/results/fingerprints" % args.working_path)
+        ifp_output = "%s/results/fingerprints/ifp.csv" % args.working_path
 
     pli_obj.calc_ifp = True
     pli_obj.ifp_num_levels = args.ifp_num_levels
@@ -111,6 +129,7 @@ def main():
     pli_obj.ifp_length = args.ifp_length
     pli_obj.ifp_type = IFP_TYPES[args.ifp_type]
     pli_obj.ifp_output = ifp_output
+    pli_obj.out_ifp_sim_matrix = args.out_sim
 
     pli_obj.generate_ifps()
 
