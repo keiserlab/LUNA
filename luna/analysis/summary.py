3f0279b9d009797dc00291bc2e37af6575fcf2d1
Unknown <alexandrefassio@ufmg.br>
2021-12-13 17:49:15 -0300

    Improved interactions count function (count_interaction_types)
    Updates:
    
        - Added the parameter ``must_have_target``, which filter out interactions not involving the target ligand.
    
        - Parameter ``targets`` became ``compounds`` as it may be confused with the term ``target`` that is used in LUNA to indicate the compounds that are used as reference for calculating interactions.
    
        - Now, by default, count_interaction_types() accepts all interactions not defined in the ``key_map``. If an interaction should be ignored, one must provides it with the value None.
    
        - Documented function.

--- luna/analysis/summary.py
+++ luna/analysis/summary.py
@@ -25,17 +25,58 @@ def count_group_types(compound, key_map={}):
     return grp_types
 
 
-def count_interaction_types(interactions, targets=None, key_map={}):
+def count_interaction_types(interactions, must_have_target=True, compounds=None, key_map={}):
+    """Count the number of each type of interaction in ``interactions``.
 
-    interaction_types = defaultdict(int)
+    Parameters
+    ----------
+    interactions : iterable
+           An iterable object containing a sequence of interactions (``InteractionType``).
+    must_have_target : bool, optional
+           If True, count only interactions involving the target ligand. Default value is True.
+    compounds: iterable or None, optional
+           Only count interactions involving the compounds in ``compounds``.
+           Default value is None, which implies that all compounds will be considered.
+    key_map: dictionary, optional
+           A dictionary to control which interactions to count and how to aggregate them.
+           The keys are the interaction types to be considered and the values are the final interaction type,
+           which can be used to aggregate interactions. If a value is None, the interaction will be ignored.
+
+           For example, to aggregate all covalent interactions, ``key_map`` would be defined as follows:
+           .. code-block:: python
+                key_map = {"Single bond": "Covalent bond",
+                           "Double bond": "Covalent bond",
+                           "Triple bond": "Covalent bond",
+                           "Aromatic bond": "Covalent bond"}
+
+            Now, if Ionic interactions should be ignored, ``key_map`` would be defined as follows:
+            .. code-block:: python
+                key_map = {"Ionic": None}
+
+    Returns
+    -------
+    interaction_types_count : dict
+        The count of each interaction type.
+    """
+    if compounds:
+        compounds = set(compounds)
+
+    interaction_types_count = defaultdict(int)
     seen_pairs = set()
     for i in interactions:
-        contain_trgts = True
-        if targets is not None:
-            if (i.src_grp.compound not in targets and i.trgt_grp.compound not in targets):
-                contain_trgts = False
+        is_valid = False
+        if not must_have_target and compounds is None:
+            is_valid = True
+        else:
+            if must_have_target:
+                if i.src_grp.has_target() or i.trgt_grp.has_target():
+                    is_valid = True
 
-        if contain_trgts:
+            if compounds:
+                if len(i.src_grp.compounds & compounds) > 0 or len(i.trgt_grp.compounds & compounds):
+                    is_valid = True
+
+        if is_valid:
             pair_key1 = (i.type, i.src_grp, i.trgt_grp)
             pair_key2 = (i.type, i.trgt_grp, i.src_grp)
 
@@ -46,10 +87,11 @@ def count_interaction_types(interactions, targets=None, key_map={}):
             key = i.type
             if key_map:
                 if i.type in key_map:
+                    if key_map[i.type] is None:
+                        continue
                     key = key_map[i.type]
                 else:
-                    continue
+                    key = i.type
 
-            interaction_types[key] += 1
-
-    return interaction_types
+            interaction_types_count[key] += 1
+    return interaction_types_count
