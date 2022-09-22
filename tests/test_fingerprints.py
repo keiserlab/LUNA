fd8d4fa5e119af6f8ace32b0321ec92eb96e3b31
Alexandre Fassio <alexandrefassio@ufmg.br>
2022-09-22 16:41:03 -0300

    Updated to new LUNA version

--- tests/test_fingerprints.py
+++ tests/test_fingerprints.py
@@ -76,7 +76,8 @@ class FingerprintTest(unittest.TestCase):
 
         pli_obj = self._get_project_results()
 
-        self.assertEqual(expected_entries, set([e.to_string() for e in pli_obj.entries]))
+        self.assertEqual(expected_entries,
+                         set([e.to_string() for e in pli_obj.entries]))
 
         create_directory(ifp_path)
 
@@ -86,35 +87,37 @@ class FingerprintTest(unittest.TestCase):
 
         self.assertRaises(FileNotFoundError, open, ifp_out1, "r")
 
+        pli_obj.calc_ifp = True
         pli_obj.ifp_output = ifp_out1
-        pli_obj.generate_ifps()
-
-        self.assertTrue(exists(ifp_out1), "File '%s' was not found." % ifp_out1)
+        pli_obj.generate_fps()
+        self.assertTrue(exists(ifp_out1),
+                        "File '%s' was not found." % ifp_out1)
 
         expected_output = StringIO('ligand_id,on_bits,count\nprotein:ZINC000012442563,83\t84\t87\t135\t157\t180\t187\t211\t273\t306\t349\t414\t520\t542\t592\t598\t604\t615\t668\t705\t764\t807\t820\t882\t911\t926\t931\t1020\t1058\t1095\t1164\t1189\t1210\t1211\t1215\t1218\t1226\t1261\t1327\t1337\t1392\t1394\t1412\t1423\t1471\t1501\t1543\t1561\t1590\t1629\t1653\t1668\t1694\t1719\t1740\t1773\t1837\t1901\t1904\t1912\t1928\t2042\t2062\t2067\t2077\t2093\t2135\t2141\t2210\t2230\t2254\t2352\t2422\t2433\t2441\t2453\t2455\t2487\t2535\t2537\t2593\t2609\t2635\t2657\t2664\t2696\t2733\t2758\t2763\t2769\t2788\t2793\t2798\t2879\t2894\t2914\t2937\t3015\t3021\t3072\t3107\t3109\t3111\t3117\t3126\t3148\t3155\t3163\t3194\t3210\t3257\t3266\t3276\t3280\t3299\t3395\t3431\t3465\t3482\t3491\t3513\t3582\t3642\t3658\t3682\t3683\t3702\t3707\t3722\t3762\t3820\t3823\t3871\t3887\t3896\t3908\t3937\t4003\t4007\t4021\t4035\t4047\t4048\t4056\t4062,2\t4\t1\t1\t27\t1\t1\t1\t1\t1\t1\t1\t1\t1\t45\t2\t1\t2\t3\t2\t4\t5\t1\t53\t20\t2\t1\t5\t1\t2\t38\t1\t37\t9\t3\t2\t1\t3\t2\t1\t1\t1\t1\t53\t1\t1\t1\t1\t1\t4\t2\t1\t1\t2\t1\t1\t42\t1\t2\t1\t2\t2\t1\t18\t2\t2\t2\t42\t3\t9\t41\t1\t13\t5\t1\t1\t1\t7\t44\t1\t1\t3\t4\t1\t1\t1\t1\t2\t1\t1\t1\t3\t2\t37\t1\t1\t2\t1\t2\t1\t1\t32\t5\t5\t3\t1\t1\t1\t3\t4\t1\t18\t1\t1\t2\t9\t1\t2\t1\t38\t1\t3\t1\t1\t1\t1\t1\t2\t5\t1\t1\t1\t1\t1\t39\t5\t3\t1\t1\t1\t1\t4\t3\t3\t39\nprotein:ZINC000343043015,3\t36\t83\t84\t157\t180\t211\t243\t349\t389\t423\t542\t547\t573\t592\t596\t598\t604\t615\t629\t668\t688\t753\t764\t797\t807\t820\t882\t911\t919\t926\t931\t962\t988\t1012\t1020\t1095\t1164\t1210\t1211\t1213\t1215\t1218\t1261\t1300\t1327\t1380\t1394\t1412\t1423\t1424\t1492\t1543\t1561\t1629\t1653\t1719\t1773\t1837\t1899\t1912\t2042\t2052\t2067\t2093\t2115\t2138\t2141\t2149\t2210\t2230\t2254\t2311\t2319\t2346\t2352\t2422\t2427\t2433\t2453\t2487\t2535\t2537\t2625\t2635\t2654\t2664\t2755\t2758\t2763\t2788\t2793\t2879\t2914\t2924\t2937\t3015\t3021\t3095\t3109\t3111\t3117\t3126\t3185\t3210\t3266\t3299\t3318\t3395\t3431\t3465\t3482\t3491\t3513\t3516\t3582\t3707\t3722\t3887\t3896\t3908\t3937\t3999\t4047\t4048\t4056\t4062,1\t1\t2\t3\t25\t2\t1\t1\t2\t1\t1\t2\t1\t1\t35\t1\t2\t2\t2\t1\t3\t1\t1\t3\t1\t5\t1\t49\t19\t1\t2\t2\t1\t1\t1\t4\t1\t33\t34\t8\t1\t2\t1\t2\t1\t2\t1\t1\t1\t49\t1\t1\t1\t1\t3\t2\t3\t1\t37\t2\t1\t3\t1\t14\t1\t1\t1\t37\t1\t3\t9\t36\t1\t1\t1\t1\t12\t2\t5\t1\t7\t40\t1\t1\t3\t1\t1\t1\t2\t1\t1\t2\t33\t1\t1\t1\t1\t2\t1\t30\t4\t4\t3\t1\t3\t14\t3\t1\t5\t1\t3\t2\t33\t1\t1\t2\t1\t4\t1\t34\t4\t2\t1\t3\t3\t2\t33\n')
         expected_df = pd.read_csv(expected_output)
         curr_df = pd.read_csv(ifp_out1)
-
         self.assertTrue(expected_df.equals(curr_df))
 
+        pli_obj.calc_ifp = True
         pli_obj.ifp_length = 4
-        pli_obj.generate_ifps()
+        pli_obj.generate_fps()
         curr_df = pd.read_csv(ifp_out1)
-
         self.assertFalse(expected_df.equals(curr_df))
 
     def test_manual_ifps_creation(self):
 
         pli_obj = self._get_project_results()
 
-        self.assertEqual(expected_entries, set([e.to_string() for e in pli_obj.entries]))
+        self.assertEqual(expected_entries,
+                         set([e.to_string() for e in pli_obj.entries]))
 
-        expected_results = {
-            "atm_grps_len": [("protein:ZINC000012442563", 485), ("protein:ZINC000343043015", 422)],
-            "inter_len": [("protein:ZINC000012442563", 73), ("protein:ZINC000343043015", 52)],
-            "ifps": ['protein:ZINC000012442563,83\t84\t87\t135\t157\t180\t187\t211\t273\t306\t349\t414\t520\t542\t592\t598\t604\t615\t668\t705\t764\t807\t820\t882\t911\t926\t931\t1020\t1058\t1095\t1164\t1189\t1210\t1211\t1215\t1218\t1226\t1261\t1327\t1337\t1392\t1394\t1412\t1423\t1471\t1501\t1543\t1561\t1590\t1629\t1653\t1668\t1694\t1719\t1740\t1773\t1837\t1901\t1904\t1912\t1928\t2042\t2062\t2067\t2077\t2093\t2135\t2141\t2210\t2230\t2254\t2352\t2422\t2433\t2441\t2453\t2455\t2487\t2535\t2537\t2593\t2609\t2635\t2657\t2664\t2696\t2733\t2758\t2763\t2769\t2788\t2793\t2798\t2879\t2894\t2914\t2937\t3015\t3021\t3072\t3107\t3109\t3111\t3117\t3126\t3148\t3155\t3163\t3194\t3210\t3257\t3266\t3276\t3280\t3299\t3395\t3431\t3465\t3482\t3491\t3513\t3582\t3642\t3658\t3682\t3683\t3702\t3707\t3722\t3762\t3820\t3823\t3871\t3887\t3896\t3908\t3937\t4003\t4007\t4021\t4035\t4047\t4048\t4056\t4062,2\t4\t1\t1\t27\t1\t1\t1\t1\t1\t1\t1\t1\t1\t45\t2\t1\t2\t3\t2\t4\t5\t1\t53\t20\t2\t1\t5\t1\t2\t38\t1\t37\t9\t3\t2\t1\t3\t2\t1\t1\t1\t1\t53\t1\t1\t1\t1\t1\t4\t2\t1\t1\t2\t1\t1\t42\t1\t2\t1\t2\t2\t1\t18\t2\t2\t2\t42\t3\t9\t41\t1\t13\t5\t1\t1\t1\t7\t44\t1\t1\t3\t4\t1\t1\t1\t1\t2\t1\t1\t1\t3\t2\t37\t1\t1\t2\t1\t2\t1\t1\t32\t5\t5\t3\t1\t1\t1\t3\t4\t1\t18\t1\t1\t2\t9\t1\t2\t1\t38\t1\t3\t1\t1\t1\t1\t1\t2\t5\t1\t1\t1\t1\t1\t39\t5\t3\t1\t1\t1\t1\t4\t3\t3\t39',
-                     'protein:ZINC000343043015,3\t36\t83\t84\t157\t180\t211\t243\t349\t389\t423\t542\t547\t573\t592\t596\t598\t604\t615\t629\t668\t688\t753\t764\t797\t807\t820\t882\t911\t919\t926\t931\t962\t988\t1012\t1020\t1095\t1164\t1210\t1211\t1213\t1215\t1218\t1261\t1300\t1327\t1380\t1394\t1412\t1423\t1424\t1492\t1543\t1561\t1629\t1653\t1719\t1773\t1837\t1899\t1912\t2042\t2052\t2067\t2093\t2115\t2138\t2141\t2149\t2210\t2230\t2254\t2311\t2319\t2346\t2352\t2422\t2427\t2433\t2453\t2487\t2535\t2537\t2625\t2635\t2654\t2664\t2755\t2758\t2763\t2788\t2793\t2879\t2914\t2924\t2937\t3015\t3021\t3095\t3109\t3111\t3117\t3126\t3185\t3210\t3266\t3299\t3318\t3395\t3431\t3465\t3482\t3491\t3513\t3516\t3582\t3707\t3722\t3887\t3896\t3908\t3937\t3999\t4047\t4048\t4056\t4062,1\t1\t2\t3\t25\t2\t1\t1\t2\t1\t1\t2\t1\t1\t35\t1\t2\t2\t2\t1\t3\t1\t1\t3\t1\t5\t1\t49\t19\t1\t2\t2\t1\t1\t1\t4\t1\t33\t34\t8\t1\t2\t1\t2\t1\t2\t1\t1\t1\t49\t1\t1\t1\t1\t3\t2\t3\t1\t37\t2\t1\t3\t1\t14\t1\t1\t1\t37\t1\t3\t9\t36\t1\t1\t1\t1\t12\t2\t5\t1\t7\t40\t1\t1\t3\t1\t1\t1\t2\t1\t1\t2\t33\t1\t1\t1\t1\t2\t1\t30\t4\t4\t3\t1\t3\t14\t3\t1\t5\t1\t3\t2\t33\t1\t1\t2\t1\t4\t1\t34\t4\t2\t1\t3\t3\t2\t33']
-        }
+        expected_results = {"atm_grps_len": [("protein:ZINC000012442563",
+                                              485),
+                                             ("protein:ZINC000343043015",
+                                              422)],
+                            "inter_len": [("protein:ZINC000012442563", 73), ("protein:ZINC000343043015", 52)],
+                            "ifps": ['protein:ZINC000012442563,83\t84\t87\t135\t157\t180\t187\t211\t273\t306\t349\t414\t520\t542\t592\t598\t604\t615\t668\t705\t764\t807\t820\t882\t911\t926\t931\t1020\t1058\t1095\t1164\t1189\t1210\t1211\t1215\t1218\t1226\t1261\t1327\t1337\t1392\t1394\t1412\t1423\t1471\t1501\t1543\t1561\t1590\t1629\t1653\t1668\t1694\t1719\t1740\t1773\t1837\t1901\t1904\t1912\t1928\t2042\t2062\t2067\t2077\t2093\t2135\t2141\t2210\t2230\t2254\t2352\t2422\t2433\t2441\t2453\t2455\t2487\t2535\t2537\t2593\t2609\t2635\t2657\t2664\t2696\t2733\t2758\t2763\t2769\t2788\t2793\t2798\t2879\t2894\t2914\t2937\t3015\t3021\t3072\t3107\t3109\t3111\t3117\t3126\t3148\t3155\t3163\t3194\t3210\t3257\t3266\t3276\t3280\t3299\t3395\t3431\t3465\t3482\t3491\t3513\t3582\t3642\t3658\t3682\t3683\t3702\t3707\t3722\t3762\t3820\t3823\t3871\t3887\t3896\t3908\t3937\t4003\t4007\t4021\t4035\t4047\t4048\t4056\t4062,2\t4\t1\t1\t27\t1\t1\t1\t1\t1\t1\t1\t1\t1\t45\t2\t1\t2\t3\t2\t4\t5\t1\t53\t20\t2\t1\t5\t1\t2\t38\t1\t37\t9\t3\t2\t1\t3\t2\t1\t1\t1\t1\t53\t1\t1\t1\t1\t1\t4\t2\t1\t1\t2\t1\t1\t42\t1\t2\t1\t2\t2\t1\t18\t2\t2\t2\t42\t3\t9\t41\t1\t13\t5\t1\t1\t1\t7\t44\t1\t1\t3\t4\t1\t1\t1\t1\t2\t1\t1\t1\t3\t2\t37\t1\t1\t2\t1\t2\t1\t1\t32\t5\t5\t3\t1\t1\t1\t3\t4\t1\t18\t1\t1\t2\t9\t1\t2\t1\t38\t1\t3\t1\t1\t1\t1\t1\t2\t5\t1\t1\t1\t1\t1\t39\t5\t3\t1\t1\t1\t1\t4\t3\t3\t39',
+                                     'protein:ZINC000343043015,3\t36\t83\t84\t157\t180\t211\t243\t349\t389\t423\t542\t547\t573\t592\t596\t598\t604\t615\t629\t668\t688\t753\t764\t797\t807\t820\t882\t911\t919\t926\t931\t962\t988\t1012\t1020\t1095\t1164\t1210\t1211\t1213\t1215\t1218\t1261\t1300\t1327\t1380\t1394\t1412\t1423\t1424\t1492\t1543\t1561\t1629\t1653\t1719\t1773\t1837\t1899\t1912\t2042\t2052\t2067\t2093\t2115\t2138\t2141\t2149\t2210\t2230\t2254\t2311\t2319\t2346\t2352\t2422\t2427\t2433\t2453\t2487\t2535\t2537\t2625\t2635\t2654\t2664\t2755\t2758\t2763\t2788\t2793\t2879\t2914\t2924\t2937\t3015\t3021\t3095\t3109\t3111\t3117\t3126\t3185\t3210\t3266\t3299\t3318\t3395\t3431\t3465\t3482\t3491\t3513\t3516\t3582\t3707\t3722\t3887\t3896\t3908\t3937\t3999\t4047\t4048\t4056\t4062,1\t1\t2\t3\t25\t2\t1\t1\t2\t1\t1\t2\t1\t1\t35\t1\t2\t2\t2\t1\t3\t1\t1\t3\t1\t5\t1\t49\t19\t1\t2\t2\t1\t1\t1\t4\t1\t33\t34\t8\t1\t2\t1\t2\t1\t2\t1\t1\t1\t49\t1\t1\t1\t1\t3\t2\t3\t1\t37\t2\t1\t3\t1\t14\t1\t1\t1\t37\t1\t3\t9\t36\t1\t1\t1\t1\t12\t2\t5\t1\t7\t40\t1\t1\t3\t1\t1\t1\t2\t1\t1\t2\t33\t1\t1\t1\t1\t2\t1\t30\t4\t4\t3\t1\t3\t14\t3\t1\t5\t1\t3\t2\t33\t1\t1\t2\t1\t4\t1\t34\t4\t2\t1\t3\t3\t2\t33']}
 
         results = list(pli_obj.results)
 
