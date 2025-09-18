8154195265634974c75f87859f6d8c97fdadfca6
Alexandre Fassio <alexandrefassio@ufmg.br>
2025-09-17 22:55:08 -0400

    If an id is not provided, use the filename as id

--- luna/pdb/parser/helpers.py
+++ luna/pdb/parser/helpers.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 from io import StringIO
 
 from luna.pdb.parser.base import PDBParser
@@ -8,17 +9,17 @@ from luna.util.exceptions import PDBNotReadError
 logger = logging.getLogger()
 
 
-def load_from_file(pdb_id, file):
+def load_from_file(pdb_file, pdb_id=None):
     """
     Load a PDB structure from file using the default PDBParser configuration.
 
     Parameters
     ----------
-    pdb_id : str
-        The structure identifier.
-    file : str or Path
+    pdb_file : str or Path
         Path to the PDB file.
-
+    pdb_id : str or None
+        The structure identifier. If None, the filename will be used as id.
+ 
     Returns
     -------
     Structure
@@ -31,16 +32,18 @@ def load_from_file(pdb_id, file):
     """
     
     try:
+        pdb_id = pdb_id or Path(pdb_file).stem
+
         parser = PDBParser(PERMISSIVE=True, 
                            QUIET=True, 
                            FIX_EMPTY_CHAINS=True,
                            FIX_ATOM_NAME_CONFLICT=True, 
                            FIX_OBABEL_FLAGS=False)
-        return parser.get_structure(pdb_id, file)
+        return parser.get_structure(pdb_id, pdb_file)
         
     except Exception as e:
-        logger.exception(f"Failed to parse file '{file}': {str(e)}")
-        raise PDBNotReadError(f"File '{file}' could not be parsed.")
+        logger.exception(f"Failed to parse file '{pdb_file}': {str(e)}")
+        raise PDBNotReadError(f"File '{pdb_file}' could not be parsed.")
 
 
 def load_from_string(pdb_id: str, pdb_string: str):
